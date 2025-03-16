858a752947c7e952300657e6f9af787b6943980e
vinuth <vinuthsriarampath@outlook.com>
2025-03-16 20:34:27 +0530

    feature: add FormsModule to app component for form handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,12 @@
  */
 
 import { AfterViewInit, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
