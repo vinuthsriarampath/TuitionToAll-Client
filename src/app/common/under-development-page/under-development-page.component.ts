0062dab9f3764e26523175c77ed3981b05749479
vinuth <vinuthsriarampath@outlook.com>
2025-03-16 20:38:13 +0530

    feature: add UnderDevelopmentPage component with placeholder content

---
+++ src/app/common/under-development-page/under-development-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-under-development-page',
+  imports: [],
+  templateUrl: './under-development-page.component.html',
+  styleUrl: './under-development-page.component.css'
+})
+export class UnderDevelopmentPageComponent {
+
+}
