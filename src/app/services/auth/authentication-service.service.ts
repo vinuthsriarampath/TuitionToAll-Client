06806f9452dac50fb7abc643ab95ad91f5cfe33f
vinuth <vinuthsriarampath@outlook.com>
2025-03-19 14:15:23 +0530

    feature: add registration methods for student, teacher, and institute in authentication service

--- src/app/services/auth/authentication-service.service.ts
+++ src/app/services/auth/authentication-service.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@angular/core';
 import { UserLoginRequest } from '../../requestDtos/user-login-request';
 import { environment } from '../../environment/environment.development';
 import { AuthResponse } from '../../responseDtos/auth-response';
+import { StudentRegistrationRequest } from '../../requestDtos/registration/student-registration-request';
+import { TeacherRegistrationRequest } from '../../requestDtos/registration/teacher-registration-request';
+import { InstituteRegistrationRequest } from '../../requestDtos/registration/institute-registration-request';
+import { ApiResponse } from '../../responseDtos/api-response';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +18,17 @@ export class AuthenticationServiceService {
   login(UserLoginRequest:UserLoginRequest){
     
     return this.http.post<AuthResponse>(`${environment.AUTH_API}/login`, UserLoginRequest)
+  }
+
+  registerStudent(StudentRegistrationRequest:StudentRegistrationRequest){
+    return this.http.post<ApiResponse>(`${environment.AUTH_API}/register/student`, StudentRegistrationRequest);
+  }
+
+  registerTeacher(TeacherRegistrationRequest:TeacherRegistrationRequest){
+    return this.http.post<ApiResponse>(`${environment.AUTH_API}/register/teacher`, TeacherRegistrationRequest);
+  }
+
+  registerInstitute(InstituteRegistrationRequest:InstituteRegistrationRequest){
+    return this.http.post<ApiResponse>(`${environment.AUTH_API}/register/institute`, InstituteRegistrationRequest);
   }
 }
