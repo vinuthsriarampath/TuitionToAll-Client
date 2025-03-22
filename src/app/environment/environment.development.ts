7fbdddc88ac396cfc980f13756c70d0828c54bec
vinuth <vinuthsriarampath@outlook.com>
2025-03-23 00:29:24 +0530

    fix: update AUTH_API endpoint for development environment

--- src/app/environment/environment.development.ts
+++ src/app/environment/environment.development.ts
@@ -3,5 +3,5 @@ interface Environment {
 }
 
 export const environment: Environment = {
-    AUTH_API: 'http://localhost:8080/api/v2/auth'
+    AUTH_API: 'http://tuitiontoall-production.up.railway.app:8080/api/v2/auth'
 };
