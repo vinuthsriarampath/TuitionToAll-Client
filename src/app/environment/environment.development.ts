0646ecb0eacac8f16659d99953a063e0151056c7
vinuth <vinuthsriarampath@outlook.com>
2025-04-10 13:28:12 +0530

    fix: update AUTH_API endpoint for development environment to use local base URL

--- src/app/environment/environment.development.ts
+++ src/app/environment/environment.development.ts
@@ -1,7 +1,11 @@
 interface Environment {
-    AUTH_API: string;
+  AUTH_API: string;
 }
 
+const P_BASE_URL = 'https://tuitiontoall-production.up.railway.app';
+const D_BASE_URL = 'http://localhost:8080';
+
 export const environment: Environment = {
-    AUTH_API: 'https://tuitiontoall-production.up.railway.app/api/v2/auth'
+  AUTH_API: `${D_BASE_URL}/api/v2/auth`,
 };
+
