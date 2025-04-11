dafd2a1c293e04cb888d0712320495cfa9e370c6
vinuth <vinuthsriarampath@outlook.com>
2025-04-11 09:49:09 +0530

    feature : add authInterceptor.ts to modify all the http request to add the Authorization to the header

---
+++ src/app/services/interceptors/authInterceptor.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2025 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ */
+
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { Injectable} from '@angular/core';
+import {AuthenticationService} from '../auth/authentication.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private readonly authService: AuthenticationService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authService.getAuthToken();
+    if (token) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+    return next.handle(req);
+  }
+}
