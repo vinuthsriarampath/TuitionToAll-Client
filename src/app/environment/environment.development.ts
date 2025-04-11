41dae67345aac64113e9a1796d055c325b1e04f2
vinuth <vinuthsriarampath@outlook.com>
2025-04-11 09:55:12 +0530

    feature: add base api url for users

--- src/app/environment/environment.development.ts
+++ src/app/environment/environment.development.ts
@@ -11,6 +11,7 @@
 
 interface Environment {
   AUTH_API: string;
+  USER_API:string;
 }
 
 const P_BASE_URL = 'https://tuitiontoall-production.up.railway.app';
@@ -18,5 +19,6 @@ const D_BASE_URL = 'http://localhost:8080';
 
 export const environment: Environment = {
   AUTH_API: `${D_BASE_URL}/api/v2/auth`,
+  USER_API: `${D_BASE_URL}/api/v2/users`
 };
 
