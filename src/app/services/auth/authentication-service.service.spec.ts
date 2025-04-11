fd3ca2385c4cd41986aa18edccb5f6171981f2f5
vinuth <vinuthsriarampath@outlook.com>
2025-04-11 09:54:09 +0530

    feature: add copyright comments

--- src/app/services/auth/authentication-service.service.spec.ts
+++ src/app/services/auth/authentication-service.service.spec.ts
@@ -1,13 +1,24 @@
+/*
+ * Copyright (c) 2025 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ */
+
 import { TestBed } from '@angular/core/testing';
 
-import { AuthenticationServiceService } from './authentication-service.service';
+import { AuthenticationService } from './authentication.service';
 
 describe('AuthenticationServiceService', () => {
-  let service: AuthenticationServiceService;
+  let service: AuthenticationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthenticationServiceService);
+    service = TestBed.inject(AuthenticationService);
   });
 
   it('should be created', () => {
--- src/app/services/auth/guard/auth.guard.spec.ts
+++ src/app/services/auth/guard/auth.guard.spec.ts
@@ -1,10 +1,21 @@
+/*
+ * Copyright (c) 2025 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ */
+
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
 import { authGuard } from './auth.guard';
 
 describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
+  const executeGuard: CanActivateFn = (...guardParameters) =>
       TestBed.runInInjectionContext(() => authGuard(...guardParameters));
 
   beforeEach(() => {
