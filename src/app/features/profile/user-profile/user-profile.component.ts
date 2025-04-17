263afb8ce2b8b57d389fbf4d3db808a4d813c144
vinuth <vinuthsriarampath@outlook.com>
2025-04-17 16:44:22 +0530

    feature : add user profile component with initial setup with routes

---
+++ src/app/features/profile/user-profile/user-profile.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NavbarComponent} from '../../../common/navbar/navbar.component';
+
+@Component({
+  selector: 'app-user-profile',
+  imports: [
+    NavbarComponent
+  ],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+
+}
