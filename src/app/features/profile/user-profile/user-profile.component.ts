694f9c2f7259c1dc7c80c3b7d999cfe8df42d7a5
vinuth <vinuthsriarampath@outlook.com>
2025-04-18 08:54:24 +0530

    feature: add basic layout for a profile page content section

--- src/app/features/profile/user-profile/user-profile.component.ts
+++ src/app/features/profile/user-profile/user-profile.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {NavbarComponent} from '../../../common/navbar/navbar.component';
+import {isInstitute, isStudent, isTeacher} from '../../../core/helpers/user/user-type-guards';
+import {Institute} from '../../../models/userModels/institute';
+import {Teacher} from '../../../models/userModels/teacher';
+import {Student} from '../../../models/userModels/student';
+import {AuthenticationService} from '../../../services/auth/authentication.service';
+import {User} from '../../../models/userModels/user';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,5 +16,40 @@ import {NavbarComponent} from '../../../common/navbar/navbar.component';
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+  userRole:string = '';
+  instituteDetails?:Institute;
+  teacherDetails?:Teacher;
+  studentDetails?:Student;
+  authService: AuthenticationService = inject(AuthenticationService);
 
+  displayName: string = '';
+  email: string = '';
+
+
+  constructor(authService:AuthenticationService) {
+    this.authService.verifyToken().subscribe({
+      next: (res) => {
+        const user :User = res.data!;
+        this.email = user.email ?? '';
+
+        if (isInstitute(user)) {
+          this.userRole = 'Institute';
+          this.instituteDetails = user;
+          this.displayName = user.instituteName ?? '';
+        } else if (isStudent(user)) {
+          this.userRole = 'Student';
+          this.studentDetails = user;
+          this.displayName = `${user.firstName ?? ''} ${user.lastName ?? ''}`.trim();
+        } else if (isTeacher(user)) {
+          this.userRole = 'Teacher';
+          this.teacherDetails = user;
+          this.displayName = `${user.firstName ?? ''} ${user.lastName ?? ''}`.trim();
+        }
+      },
+      error(err){
+        console.error(err);
+        window.location.replace('/dashboard')
+      }
+    })
+  }
 }
