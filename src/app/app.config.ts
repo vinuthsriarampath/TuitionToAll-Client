528aba210a8432a249a977d26f5ef36e2cc1ddd9
vinuth <vinuthsriarampath@outlook.com>
2025-05-01 08:26:02 +0530

    refactor: update import paths for AuthInterceptor and AuthenticationService

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,7 +14,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
-import {AuthInterceptor} from './services/interceptors/authInterceptor';
+import {AuthInterceptor} from './core/interceptors/authInterceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
