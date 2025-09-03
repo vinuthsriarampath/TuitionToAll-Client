1e4b462cd5ac46a42dc2128d371341aa4746174d
vinuth <vinuthsriarampath@outlook.com>
2025-09-03 12:08:04 +0530

    feat: implemented institute course management component for course visualization

--- src/app/features/dashboards/institute-dashboard/pages/institute-course-management/institute-course-management.component.ts
+++ src/app/features/dashboards/institute-dashboard/pages/institute-course-management/institute-course-management.component.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {CourseService} from '../../../../../core/services/course/course.service';
+import {Course} from '../../../../../core/models/course';
+import {AlertService} from '../../../../../core/services/alerts/alert.service';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {CurrencyPipe, NgClass} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-institute-course-management',
-  imports: [],
+  imports: [
+    MatTableModule,
+    NgClass,
+    CurrencyPipe,
+    MatPaginatorModule,
+    RouterLink,
+  ],
   templateUrl: './institute-course-management.component.html',
   styleUrl: './institute-course-management.component.css'
 })
-export class InstituteCourseManagementComponent {
+export class InstituteCourseManagementComponent implements AfterViewInit {
+  courses: Course[] = [];
+  dataSource = new MatTableDataSource<Course>();
+  loading: boolean = false;
 
+  displayedColumns: string[] = [ 'id', 'title', 'category', 'durationInHours', 'price', 'status'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+  constructor(
+    private readonly courseService: CourseService,
+    private readonly alertService: AlertService
+  ) {
+    this.fetchCourses();
+  }
+
+  ngAfterViewInit(): void {
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  fetchCourses(): void {
+    this.triggerLoading();
+    this.dataSource.data = [{} as Course];
+    this.courseService.getInstitute().subscribe({
+      next: (res) => {
+        this.courses = Array.isArray(res.data) ? res.data : [];
+        this.dataSource.data = this.courses;
+        this.triggerLoading();
+      },
+      error: (err) => {
+        this.alertService.triggerErrorAlert(err.error.message);
+        this.courses = [];
+        this.dataSource.data = [{} as Course];
+        this.triggerLoading();
+      }
+    });
+  }
+
+  triggerLoading(): void {
+    this.loading = !this.loading;
+  }
 }
