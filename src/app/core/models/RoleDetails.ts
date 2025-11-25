72943d9d2f1454718203459b456a2e2829f7538b
vinuth <vinuthsriarampath@outlook.com>
2025-11-25 08:07:34 +0530

    feat: add RoleDetails type to manage user roles

---
+++ src/app/core/models/RoleDetails.ts
@@ -0,0 +1,5 @@
+import {Institute} from './user-models/institute';
+import {Teacher} from './user-models/teacher';
+import {Student} from './user-models/student';
+
+export type RoleDetails = Student | Teacher | Institute;
