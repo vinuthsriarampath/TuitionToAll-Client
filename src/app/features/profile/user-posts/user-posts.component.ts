db99f112460b5c9da4407a262272ca776fc46a7d
vinuth <vinuthsriarampath@outlook.com>
2025-11-29 14:13:22 +0530

    feat: add sample user posts component

---
+++ src/app/features/profile/user-posts/user-posts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-posts',
+  imports: [],
+  templateUrl: './user-posts.component.html',
+  styleUrl: './user-posts.component.css'
+})
+export class UserPostsComponent {
+
+}
