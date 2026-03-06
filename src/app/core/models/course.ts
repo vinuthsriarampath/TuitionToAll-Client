932349e8793fe8f7b3051dfd7f8f9b15b91bc00c
vinuth <vinuthsriarampath@outlook.com>
2026-03-06 20:33:31 +0530

    refactor: add avg_rating and total_no_ratings fields to course model; refactor the course model with optional properties

--- src/app/core/models/course.ts
+++ src/app/core/models/course.ts
@@ -4,18 +4,20 @@ import {CourseStatus} from '../enums/course-status';
 import {CourseLanguage} from '../enums/course-language';
 import {CourseMode} from '../enums/course-mode';
 
-export interface Course{
-  id:number;
-  title:string;
-  description:string;
-  durationInHours:number;
-  price:number;
-  level:CourseLevel;
-  category:CourseCategory;
-  status:CourseStatus;
-  language:CourseLanguage;
-  mode:CourseMode;
-  thumbnail:string;
-  creationTimeStamp:string;
-  updatedAt:string;
+export class Course{
+  id?:number;
+  title?:string;
+  description?:string;
+  durationInHours?:number;
+  price?:number;
+  level?:CourseLevel;
+  category?:CourseCategory;
+  status?:CourseStatus;
+  language?:CourseLanguage;
+  mode?:CourseMode;
+  thumbnail?:string;
+  avg_rating?:number;
+  total_no_ratings?:number;
+  creationTimeStamp?:string;
+  updatedAt?:string;
 }
