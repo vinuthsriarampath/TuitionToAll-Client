d517f07922391193f26642fb8674c4c8635e8cda
vinuth <vinuthsriarampath@outlook.com>
2026-03-29 21:25:09 +0530

    feat: add custom validator for future date validation

---
+++ src/app/core/helpers/form-custom-validators.ts
@@ -0,0 +1,17 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+
+export function isFutureDate(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (!control.value) return null; // important (let required handle empty)
+
+    const selectedDate = new Date(control.value);
+    const now = new Date();
+    return selectedDate > now
+      ? null
+      : {
+        isFutureDate: {
+          message: 'Date must be in the future'
+        }
+      };
+  };
+}
