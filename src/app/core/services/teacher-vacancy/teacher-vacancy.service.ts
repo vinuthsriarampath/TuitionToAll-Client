b64758f8dc0ecc4e25f52ac2ca1a4c9375c2fbab
vinuth <vinuthsriarampath@outlook.com>
2026-03-29 21:25:57 +0530

    feat: implement TeacherVacancyService with createVacancy method and corresponding tests

---
+++ src/app/core/services/teacher-vacancy/teacher-vacancy.service.ts
@@ -0,0 +1,19 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {TeacherVacancyCreateRequest} from '../../dto/request-dto/teacher-vacancy/teacher-vacancy-create-request';
+import {Observable} from 'rxjs';
+import {ApiResponse} from '../../dto/response-dto/api-response';
+import {TeacherVacancy} from '../../models/teacher-vacancy';
+import {environment} from '../../../environment/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeacherVacancyService {
+
+  private readonly http = inject(HttpClient);
+
+  createVacancy(request:TeacherVacancyCreateRequest):Observable<ApiResponse<TeacherVacancy>>{
+    return this.http.post<ApiResponse<TeacherVacancy>>(`${environment.TEACHER_VACANCY_API}/`,request);
+  }
+}
