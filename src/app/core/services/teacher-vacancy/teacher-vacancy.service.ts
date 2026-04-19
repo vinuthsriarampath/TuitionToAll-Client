132e8688761c7b5ac711029be3f71191d291fb18
vinuth <vinuthsriarampath@outlook.com>
2026-04-19 19:11:50 +0530

    feat: update getVacanciesByInstitute method to support pagination with PaginatedApiResponse

--- src/app/core/services/teacher-vacancy/teacher-vacancy.service.ts
+++ src/app/core/services/teacher-vacancy/teacher-vacancy.service.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs';
 import {ApiResponse} from '../../dto/response-dto/api-response';
 import {TeacherVacancy} from '../../models/teacher-vacancy';
 import {environment} from '../../../environment/environment.development';
+import {PaginatedApiResponse} from '../../dto/response-dto/paginated-api-response';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +18,8 @@ export class TeacherVacancyService {
     return this.http.post<ApiResponse<TeacherVacancy>>(`${environment.TEACHER_VACANCY_API}/`,request);
   }
 
-  getVacanciesByInstitute():Observable<ApiResponse<TeacherVacancy[]>>{
-    return this.http.get<ApiResponse<TeacherVacancy[]>>(`${environment.TEACHER_VACANCY_API}/my`);
+  getVacanciesByInstitute(pageIndex:number = 0, pageSize:number = 10):Observable<PaginatedApiResponse<TeacherVacancy>>{
+    return this.http.get<PaginatedApiResponse<TeacherVacancy>>(`${environment.TEACHER_VACANCY_API}/my?page=${pageIndex}&size=${pageSize}`);
   }
 
   updateVacancy(vacancyId:number,request:TeacherVacancyCreateRequest):Observable<ApiResponse<TeacherVacancy>>{
