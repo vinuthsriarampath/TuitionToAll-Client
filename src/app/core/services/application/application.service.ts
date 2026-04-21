5678381175d5f48c466d42bab9da50594f0d2e67
vinuth <vinuthsriarampath@outlook.com>
2026-04-21 22:08:14 +0530

    feat: add ApplicationService for managing application submissions and checks

---
+++ src/app/core/services/application/application.service.ts
@@ -0,0 +1,22 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ApiResponse} from '../../dto/response-dto/api-response';
+import {Application} from '../../models/application';
+import {environment} from '../../../environment/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApplicationService {
+
+  private readonly http:HttpClient = inject(HttpClient);
+
+  createApplication(vacancyId:number):Observable<ApiResponse<Application>>{
+    return this.http.post<ApiResponse<Application>>(`${environment.APPLICATION_API}/vacancies/${vacancyId}/apply`,null)
+  }
+
+  checkIfUserAlreadyApplied(teacherId:number,vacancyId:number):Observable<ApiResponse<any>>{
+    return this.http.get<ApiResponse<any>>(`${environment.APPLICATION_API}/ckeck?teacherId=${vacancyId}&vacancyId=${teacherId}`);
+  }
+}
