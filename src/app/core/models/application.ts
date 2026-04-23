b93b1b9e9b1a2520eee9a59b3cbf192a22a64e85
vinuth <vinuthsriarampath@outlook.com>
2026-04-23 21:18:09 +0530

    feat: add ApplicationStatus enum for managing application states

--- src/app/core/models/application.ts
+++ src/app/core/models/application.ts
@@ -1,7 +1,10 @@
+import {ApplicationStatus} from '../enums/application-status';
+
 export class Application {
   id!: number;
   teacherId!: number;
   teacherVacancyId!: number;
+  status!: ApplicationStatus;
   appliedDate!: Date;
   lastModifiedDate!: Date;
 }
