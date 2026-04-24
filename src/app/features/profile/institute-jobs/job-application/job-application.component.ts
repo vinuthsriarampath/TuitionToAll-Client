248cd24f499ada2548aacad355566b31f4954854
vinuth <vinuthsriarampath@outlook.com>
2026-04-24 19:47:26 +0530

    feat: add success alert for application submission with detailed information

--- src/app/features/profile/institute-jobs/job-application/job-application.component.ts
+++ src/app/features/profile/institute-jobs/job-application/job-application.component.ts
@@ -89,6 +89,10 @@ export class JobApplicationComponent implements OnInit{
     if (this.isApplied) return;
     const message =  "Apply for This Vacancy?";
     const description = "Once you apply, your application will be recorded and sent to the institute for review. You won’t be able to submit another application for this vacancy.";
+
+    const title = "Application Submitted Successfully !";
+    const informationDescription = "<div class=\"text-sm text-gray-600 space-y-3 leading-relaxed\"><p>Your application has been successfully submitted to the institute.</p><p>The institute may contact you via <strong>email</strong>, <strong>phone call</strong>, or <strong>direct messaging</strong> regarding the next steps.</p><div><p class=\"font-medium text-gray-700 mb-1\">Important Information:</p><ul class=\"list-disc pl-5 space-y-1\"><li>All interviews and evaluations are conducted directly by the institute.</li><li>TuitionToAll does not manage or participate in the selection process.</li><li>TuitionToAll is not responsible for any issues during or after the selection process.</li><li>If selected, you will receive an official email via TuitionToAll on behalf of the institute.</li><li>The institute may also contact you separately based on their procedures.</li><li>After selection, all responsibilities and workload are handled by the institute.</li></ul></div><p>You can track your application status from your dashboard.</p></div>";
+
     this.alertService.triggerSuccessConfirmationAlert(message,description).subscribe({
       next: (res)=>{
         if(res){
@@ -97,6 +101,7 @@ export class JobApplicationComponent implements OnInit{
               next: (res)=>{
                 if(res){
                   this.checkIfApplied();
+                  this.alertService.triggerInformationAlert(title,informationDescription);
                 }
               },
               error: (err)=>{
