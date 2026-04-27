8bb42064a21bf1853616e3e163f71538f0c851ba
vinuth <vinuthsriarampath@outlook.com>
2026-04-27 19:21:29 +0530

    feat: implement bulk selection and onboarding for job applications

--- src/app/features/dashboards/institute-dashboard/pages/institute-teacher-management/pages/view-teacher-vacancy/pages/view-application/view-application.component.ts
+++ src/app/features/dashboards/institute-dashboard/pages/institute-teacher-management/pages/view-teacher-vacancy/pages/view-application/view-application.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit, ViewChild, viewChild} from '@angular/core';
+import {Component, inject, OnInit, ViewChild} from '@angular/core';
 import {ApplicationService} from '../../../../../../../../../core/services/application/application.service';
 import {ActivatedRoute} from '@angular/router';
 import {
@@ -7,10 +7,15 @@ import {
 import {AlertService} from '../../../../../../../../../core/services/alerts/alert.service';
 import {PageTitleComponent} from '../../../../../../../../../shared/components/page-title/page-title.component';
 import {
-  MatCell, MatCellDef,
+  MatCell,
+  MatCellDef,
   MatColumnDef,
   MatHeaderCell,
-  MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
   MatTable,
   MatTableDataSource
 } from '@angular/material/table';
@@ -19,6 +24,11 @@ import {MatPaginator, PageEvent} from '@angular/material/paginator';
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
 import {environment} from '../../../../../../../../../environment/environment.development';
 import {ApplicationStatus} from '../../../../../../../../../core/enums/application-status';
+import {SelectionModel} from '@angular/cdk/collections';
+import {
+  InstituteTeacherService
+} from '../../../../../../../../../core/services/institute-teacher/institute-teacher.service';
+import {ApplicationSelectionRequest} from '../../../../../../../../../core/dto/request-dto/ApplicationSelectionRequest';
 
 @Component({
   selector: 'app-view-application',
@@ -52,10 +62,35 @@ export class ViewApplicationComponent implements OnInit {
 
   // Table related variables
   protected dataSource = new MatTableDataSource(this.applications);
-  protected columns: string[] = ['id', 'dp' , 'status' , 'appliedDate', 'actions'];
+  protected columns: string[] = ['select','id', 'dp' ,'firstName','lastName', 'status' , 'appliedDate', 'actions'];
   protected pageIndex:number = 0;
   protected pageSize:number = 10;
   protected totalElements:number = 0;
+
+  //multi select related variables
+  protected selection = new SelectionModel<ApplicationDetailsResponse>(true,[]);
+
+  protected get hasSelection(): boolean {
+    return this.selection.selected.length > 0;
+  }
+
+  protected toggleRow(row: ApplicationDetailsResponse): void {
+    this.selection.toggle(row);
+  }
+
+  protected isAllSelected(): boolean {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  protected toggleAll(): void {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+    } else {
+      this.dataSource.data.forEach(row => this.selection.select(row));
+    }
+  }
 
   // Drawer related variables
   @ViewChild('drawer') drawer!: MatSidenav;
@@ -65,6 +100,7 @@ export class ViewApplicationComponent implements OnInit {
   private readonly applicationService: ApplicationService = inject((ApplicationService))
   private readonly activatedRoute = inject(ActivatedRoute);
   private readonly alertService = inject(AlertService);
+  private readonly instituteTeacherService = inject(InstituteTeacherService);
 
   protected readonly environment = environment;
   protected readonly ApplicationStatus = ApplicationStatus;
@@ -102,10 +138,38 @@ export class ViewApplicationComponent implements OnInit {
     })
   }
 
+  onBulkSelect():void{
+    const request:ApplicationSelectionRequest = new ApplicationSelectionRequest();
+    request.applicationIds = this.selection.selected.map(a => a.id);
+    this.instituteTeacherService.onboardTeachers(request).subscribe({
+      next: (res)=>{
+        if(res.data){
+          this.alertService.triggerSuccessAlert(` ${res.data.successApplicationIds.length} applications successfully onboarded!`);
+
+          for (let app of this.dataSource.data){
+            if(res.data?.successApplicationIds.includes(app.id)){
+              app.status = ApplicationStatus.SELECTED;
+            }
+          }
+
+          if (res.data.failedApplicationIds.length > 0){
+            this.alertService.triggerErrorAlert(` ${res.data.failedApplicationIds.length} applications got failed to onboarded!`);
+          }
+
+          this.selection.clear();
+        }
+      },
+      error: (err)=>{
+        console.log(err.error.message)
+      }
+    })
+  }
+
   protected onPageChange(event: PageEvent):void{
     this.pageIndex = event.pageIndex;
     this.pageSize = event.pageSize;
     this.loadApplications();
+    this.selection.clear();
   }
 
   protected openDrawer(teacher:ApplicationDetailsResponse):void{
