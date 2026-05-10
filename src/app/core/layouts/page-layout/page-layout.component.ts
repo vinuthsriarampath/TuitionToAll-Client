f6984cf60815466937d63de37d32c9c0d517b586
vinuth <vinuthsriarampath@outlook.com>
2026-05-10 19:24:30 +0530

    feat: add page layout component with template, styles, and logic

---
+++ src/app/core/layouts/page-layout/page-layout.component.ts
@@ -0,0 +1,19 @@
+import {Component, input} from '@angular/core';
+import {LucideAngularModule} from 'lucide-angular';
+import {PageTitleComponent} from '../../../shared/components/page-title/page-title.component';
+
+@Component({
+  selector: 'app-page-layout',
+  imports: [
+    LucideAngularModule,
+    PageTitleComponent
+  ],
+  templateUrl: './page-layout.component.html',
+  styleUrl: './page-layout.component.css'
+})
+export class PageLayoutComponent {
+
+  headerTitle = input.required<string>();
+  headerDescription = input.required<string>();
+  showBackButton = input<boolean>(false);
+}
