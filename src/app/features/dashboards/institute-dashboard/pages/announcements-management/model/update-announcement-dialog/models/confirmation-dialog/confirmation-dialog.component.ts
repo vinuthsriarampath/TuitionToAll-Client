0bd5125aab8c3930d6424f456a8695ba6b730529
vinuth <vinuthsriarampath@outlook.com>
2026-05-11 08:51:52 +0530

    feat: refactor confirmation dialog to use dialog layout component for improved structure and consistency

--- src/app/features/dashboards/institute-dashboard/pages/announcements-management/model/update-announcement-dialog/models/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/features/dashboards/institute-dashboard/pages/announcements-management/model/update-announcement-dialog/models/confirmation-dialog/confirmation-dialog.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, inject} from '@angular/core';
 import {LucideAngularModule, LucideIconData} from 'lucide-angular';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {NgClass} from '@angular/common';
+import {DialogLayoutComponent} from '../../../../../../../../../core/layouts/dialog-layout/dialog-layout.component';
 
 export interface ConfirmationDialogData {
   title: string;
@@ -21,7 +22,8 @@ export interface ConfirmationDialogData {
   selector: 'app-confirmation-dialog',
   imports: [
     LucideAngularModule,
-    NgClass
+    NgClass,
+    DialogLayoutComponent
   ],
   templateUrl: './confirmation-dialog.component.html',
   styleUrl: './confirmation-dialog.component.css'
