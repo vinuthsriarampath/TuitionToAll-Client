94882cf29168cc37fb9e6e7dff2cedadf954e5e3
vinuth <vinuthsriarampath@outlook.com>
2026-05-11 23:47:01 +0530

    feat: add card shell component with template and styles for improved layout structure

---
+++ src/app/shared/ui/card-shell/card-shell.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-shell',
+  imports: [],
+  templateUrl: './card-shell.component.html',
+  styleUrl: './card-shell.component.css'
+})
+export class CardShellComponent {
+
+}
