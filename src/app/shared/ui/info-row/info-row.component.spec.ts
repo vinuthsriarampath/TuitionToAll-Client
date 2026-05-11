ef7c4dccb8866f3060e5ce94dc9b315d0ed61406
vinuth <vinuthsriarampath@outlook.com>
2026-05-11 23:49:57 +0530

    feat: add info row component with label and value inputs for displaying information

---
+++ src/app/shared/ui/info-row/info-row.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InfoRowComponent } from './info-row.component';
+
+describe('InfoRowComponent', () => {
+  let component: InfoRowComponent;
+  let fixture: ComponentFixture<InfoRowComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [InfoRowComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InfoRowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/ui/info-row/info-row.component.ts
@@ -0,0 +1,13 @@
+import {Component, input} from '@angular/core';
+
+@Component({
+  selector: 'app-info-row',
+  imports: [],
+  templateUrl: './info-row.component.html',
+  styleUrl: './info-row.component.css'
+})
+export class InfoRowComponent {
+  label = input.required<string>();
+
+  value = input.required<string | number | undefined>();
+}
