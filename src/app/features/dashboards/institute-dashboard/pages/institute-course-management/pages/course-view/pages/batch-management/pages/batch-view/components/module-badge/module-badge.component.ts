c4874c28cc3cfb841a7396afeb189a65f678d5e4
vinuth <vinuthsriarampath@outlook.com>
2026-05-14 11:34:22 +0530

    feat: add ModuleBadgeComponent for displaying module status with dynamic styling

---
+++ src/app/features/dashboards/institute-dashboard/pages/institute-course-management/pages/course-view/pages/batch-management/pages/batch-view/components/module-badge/module-badge.component.ts
@@ -0,0 +1,17 @@
+import {Component, input} from '@angular/core';
+import {ModuleStatus} from '../../../../../../../../../../../../../core/enums/ModuleStatus';
+import {BadgeComponent} from '../../../../../../../../../../../../../shared/ui/badge/badge.component';
+
+@Component({
+  selector: 'app-module-badge',
+  imports: [
+    BadgeComponent
+  ],
+  templateUrl: './module-badge.component.html',
+  styleUrl: './module-badge.component.css'
+})
+export class ModuleBadgeComponent {
+  status = input.required<ModuleStatus>();
+
+  protected readonly ModuleStatus = ModuleStatus;
+}
