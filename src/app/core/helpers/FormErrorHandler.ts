44d9c54ca72e19b981f7e5db2961217c4a719813
vinuth <vinuthsriarampath@outlook.com>
2026-05-15 08:19:04 +0530

    feat: add FormErrorHandler service for handling form errors and displaying alerts

---
+++ src/app/core/helpers/FormErrorHandler.ts
@@ -0,0 +1,29 @@
+import {FormGroup} from '@angular/forms';
+import {AlertService} from '../services/alerts/alert.service';
+import {inject, Injectable} from '@angular/core';
+
+
+@Injectable(
+  {providedIn: 'root'}
+)
+export class FormErrorHandler {
+
+  private readonly alertService:AlertService = inject(AlertService);
+
+  public handle(err: any,form:FormGroup,triggerLoading:() => void):void {
+    const errors =  err.error?.errors;
+    triggerLoading();
+    if (errors && errors.length > 0) {
+      for (const e of errors) {
+        form.get(e.field)?.setErrors({
+          server: e.message
+        });
+      }
+    }else if(err.error?.message){
+      form.setErrors({server: err.error.message});
+    }else {
+      this.alertService.triggerErrorAlert("An unexpected error occurred");
+      form.setErrors({server: "An unexpected error occurred"});
+    }
+  }
+}
