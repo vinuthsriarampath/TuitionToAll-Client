f41d822644424b25583291c180b4817b1288a784
vinuth <vinuthsriarampath@outlook.com>
2026-05-16 20:21:51 +0530

    feat: add ModuleDangerPanelComponent for locking module access

---
+++ src/app/features/dashboards/institute-dashboard/pages/institute-course-management/pages/course-view/pages/batch-management/pages/batch-view/components/module-danger-panel/module-danger-panel.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModuleDangerPanelComponent } from './module-danger-panel.component';
+
+describe('ModuleDangerPanelComponent', () => {
+  let component: ModuleDangerPanelComponent;
+  let fixture: ComponentFixture<ModuleDangerPanelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ModuleDangerPanelComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ModuleDangerPanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
