998c988390f78ab47a7214605fabfb2fa54b1e39
vinuth <vinuthsriarampath@outlook.com>
2026-05-18 20:33:49 +0530

    feat: add ChapterUpdateDialogComponent for updating existing chapters

---
+++ src/app/features/dashboards/institute-dashboard/pages/institute-course-management/pages/course-view/pages/batch-management/pages/batch-view/components/chapter-update-dialog/chapter-update-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChapterUpdateDialogComponent } from './chapter-update-dialog.component';
+
+describe('ChapterUpdateDialogComponent', () => {
+  let component: ChapterUpdateDialogComponent;
+  let fixture: ComponentFixture<ChapterUpdateDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ChapterUpdateDialogComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ChapterUpdateDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
