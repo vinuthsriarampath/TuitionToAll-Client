a341d2d7790d78a82ec2e557cd336244de7401f5
vinuth <vinuthsriarampath@outlook.com>
2026-05-19 22:20:27 +0530

    feat: add ScheduledLecturesComponent with template and styles and added sample data temporarily

---
+++ src/app/features/dashboards/institute-dashboard/pages/institute-course-management/pages/course-view/pages/batch-management/pages/batch-view/components/scheduled-lectures/scheduled-lectures.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {CardShellComponent} from '../../../../../../../../../../../../../shared/ui/card-shell/card-shell.component';
+
+@Component({
+  selector: 'app-scheduled-lectures',
+  imports: [
+    CardShellComponent
+  ],
+  templateUrl: './scheduled-lectures.component.html',
+  styleUrl: './scheduled-lectures.component.css'
+})
+export class ScheduledLecturesComponent {
+
+}
