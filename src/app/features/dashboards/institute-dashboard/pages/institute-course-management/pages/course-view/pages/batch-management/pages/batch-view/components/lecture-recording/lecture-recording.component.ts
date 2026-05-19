adc8b50ccb308bc1d29b45589cc71f28dd406b55
vinuth <vinuthsriarampath@outlook.com>
2026-05-19 22:21:33 +0530

    feat: add LectureRecordingComponent with template, styles, and basic test setup add added sample data temporarily

---
+++ src/app/features/dashboards/institute-dashboard/pages/institute-course-management/pages/course-view/pages/batch-management/pages/batch-view/components/lecture-recording/lecture-recording.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {CardShellComponent} from '../../../../../../../../../../../../../shared/ui/card-shell/card-shell.component';
+
+@Component({
+  selector: 'app-lecture-recording',
+  imports: [
+    CardShellComponent
+  ],
+  templateUrl: './lecture-recording.component.html',
+  styleUrl: './lecture-recording.component.css'
+})
+export class LectureRecordingComponent {
+
+}
