b84fee8d4258fa7aaefe5caded9ec026a342902c
vinuth <vinuthsriarampath@outlook.com>
2026-05-19 22:21:06 +0530

    feat: add ResourcesComponent with template, styles, and basic test setup and added sample data temporarily

---
+++ src/app/features/dashboards/institute-dashboard/pages/institute-course-management/pages/course-view/pages/batch-management/pages/batch-view/components/resources/resources.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {CardShellComponent} from '../../../../../../../../../../../../../shared/ui/card-shell/card-shell.component';
+
+@Component({
+  selector: 'app-resources',
+  imports: [
+    CardShellComponent
+  ],
+  templateUrl: './resources.component.html',
+  styleUrl: './resources.component.css'
+})
+export class ResourcesComponent {
+
+}
