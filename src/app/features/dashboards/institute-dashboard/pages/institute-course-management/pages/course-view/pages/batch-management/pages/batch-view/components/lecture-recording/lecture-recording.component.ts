dd1d6eb90262e99970fa88a15608788b39e8672a
vinuth <vinuthsriarampath@outlook.com>
2026-05-21 19:41:39 +0530

    feat: implement dynamic lecture record display and fetch records by chapter ID

--- src/app/features/dashboards/institute-dashboard/pages/institute-course-management/pages/course-view/pages/batch-management/pages/batch-view/components/lecture-recording/lecture-recording.component.ts
+++ src/app/features/dashboards/institute-dashboard/pages/institute-course-management/pages/course-view/pages/batch-management/pages/batch-view/components/lecture-recording/lecture-recording.component.ts
@@ -4,11 +4,17 @@ import {MatDialog} from '@angular/material/dialog';
 import {LectureRecordUploadComponent} from '../lecture-record-upload/lecture-record-upload.component';
 import {ActivatedRoute} from '@angular/router';
 import {AlertService} from '../../../../../../../../../../../../../core/services/alerts/alert.service';
+import {
+  LectureRecordResponse
+} from '../../../../../../../../../../../../../core/dto/response-dto/lecture-record/LectureRecordResponse';
+import {ChapterService} from '../../../../../../../../../../../../../core/services/chapter/chapter.service';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-lecture-recording',
   imports: [
-    CardShellComponent
+    CardShellComponent,
+    DatePipe
   ],
   templateUrl: './lecture-recording.component.html',
   styleUrl: './lecture-recording.component.css'
@@ -16,10 +22,12 @@ import {AlertService} from '../../../../../../../../../../../../../core/services
 export class LectureRecordingComponent implements  OnInit{
 
   private chapterId!:number;
+  protected lectureRecords:LectureRecordResponse[] =[];
 
   private readonly dialog:MatDialog = inject(MatDialog);
   private readonly alertService:AlertService = inject(AlertService);
   private readonly activatedRoute:ActivatedRoute = inject(ActivatedRoute);
+  private readonly chapterService:ChapterService = inject(ChapterService);
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe((params)=>{
@@ -29,6 +37,18 @@ export class LectureRecordingComponent implements  OnInit{
         return;
       }else {
         this.chapterId = Number.parseInt(chapterIdParam);
+        this.fetchAllLectureRecords();
+      }
+    })
+  }
+
+  private fetchAllLectureRecords():void{
+    this.chapterService.getAllLectureRecordsByChapterId(this.chapterId).subscribe({
+      next:(res)=>{
+        if(res.data) this.lectureRecords = res.data
+      },
+      error:(err)=>{
+        this.alertService.triggerErrorAlert(err.error.message);
       }
     })
   }
@@ -39,6 +59,13 @@ export class LectureRecordingComponent implements  OnInit{
       data:this.chapterId
     });
 
-
+    dialogRef.afterClosed().subscribe({
+      next:(res)=>{
+        if(res) this.lectureRecords.push(res);
+      },
+      error:(err)=>{
+        this.alertService.triggerErrorAlert(err.error.message);
+      }
+    })
   }
 }
