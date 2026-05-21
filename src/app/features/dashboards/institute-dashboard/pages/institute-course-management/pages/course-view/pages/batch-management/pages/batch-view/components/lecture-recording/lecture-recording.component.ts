3885b4baeba28c76c1f5068b5ebffa0d6d355580
vinuth <vinuthsriarampath@outlook.com>
2026-05-21 13:42:31 +0530

    feat: implement lecture recording upload dialog and chapter ID validation

--- src/app/features/dashboards/institute-dashboard/pages/institute-course-management/pages/course-view/pages/batch-management/pages/batch-view/components/lecture-recording/lecture-recording.component.ts
+++ src/app/features/dashboards/institute-dashboard/pages/institute-course-management/pages/course-view/pages/batch-management/pages/batch-view/components/lecture-recording/lecture-recording.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {CardShellComponent} from '../../../../../../../../../../../../../shared/ui/card-shell/card-shell.component';
+import {MatDialog} from '@angular/material/dialog';
+import {LectureRecordUploadComponent} from '../lecture-record-upload/lecture-record-upload.component';
+import {ActivatedRoute} from '@angular/router';
+import {AlertService} from '../../../../../../../../../../../../../core/services/alerts/alert.service';
 
 @Component({
   selector: 'app-lecture-recording',
@@ -9,6 +13,32 @@ import {CardShellComponent} from '../../../../../../../../../../../../../shared/
   templateUrl: './lecture-recording.component.html',
   styleUrl: './lecture-recording.component.css'
 })
-export class LectureRecordingComponent {
+export class LectureRecordingComponent implements  OnInit{
 
+  private chapterId!:number;
+
+  private readonly dialog:MatDialog = inject(MatDialog);
+  private readonly alertService:AlertService = inject(AlertService);
+  private readonly activatedRoute:ActivatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params)=>{
+      const chapterIdParam = params.get('chapterId') ?? '';
+      if(chapterIdParam && Number.isNaN(chapterIdParam)){
+        this.alertService.triggerErrorAlert("Invalid chapter id passed via route parameters");
+        return;
+      }else {
+        this.chapterId = Number.parseInt(chapterIdParam);
+      }
+    })
+  }
+
+  protected openLectureRecordingUploadDialog():void{
+    const dialogRef = this.dialog.open(LectureRecordUploadComponent,{
+      width:'650px',
+      data:this.chapterId
+    });
+
+
+  }
 }
