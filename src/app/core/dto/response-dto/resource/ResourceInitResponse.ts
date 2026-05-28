c22c72152c8b38b3780a04ce838213b8b33eca57
vinuth <vinuthsriarampath@outlook.com>
2026-05-28 12:21:17 +0530

    feat: add resource request and response classes for chunk upload functionality

---
+++ src/app/core/dto/response-dto/resource/ResourceInitResponse.ts
@@ -0,0 +1,3 @@
+export class ResourceInitResponse {
+  uploadId!: string;
+}
---
+++ src/app/core/dto/response-dto/resource/ResourceResponse.ts
@@ -0,0 +1,8 @@
+export class ResourceResponse {
+  id!: number;
+  chapterId!: number;
+  name!: string;
+  fileName!: string;
+  createdDate!: string;
+  lastModifiedDate!: string;
+}
