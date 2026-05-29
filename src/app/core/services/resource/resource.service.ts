23a41545413913be6685735915344abdb49f62ff
vinuth <vinuthsriarampath@outlook.com>
2026-05-29 07:05:13 +0530

    fix: update uploadChunk method to support progress reporting and change file type to Blob

--- src/app/core/services/resource/resource.service.ts
+++ src/app/core/services/resource/resource.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {environment} from '../../../environment/environment.development';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpEvent} from '@angular/common/http';
 import { Observable } from "rxjs";
 import {ResourceInitRequest} from '../../dto/request-dto/resource/ResourceInitRequest';
 import { ApiResponse } from "../../dto/response-dto/api-response";
@@ -20,14 +20,14 @@ export class ResourceService {
     return this.http.post<ApiResponse<ResourceInitResponse>>(`${this.baseUrl}/upload/init`, request);
   }
 
-  uploadChunk(uploadId: string, chunkIndex: number, file: File): Observable<ApiResponse<ResourceChunkUploadResponse>> {
+  uploadChunk(uploadId: string, chunkIndex: number, file: Blob): Observable<HttpEvent<ApiResponse<ResourceChunkUploadResponse>>> {
 
     const formData = new FormData();
     formData.append('uploadId', uploadId);
     formData.append('chunkIndex', chunkIndex.toString());
     formData.append('file', file);
 
-    return this.http.post<ApiResponse<ResourceChunkUploadResponse>>(`${this.baseUrl}/upload/chunk`, formData);
+    return this.http.post<ApiResponse<ResourceChunkUploadResponse>>(`${this.baseUrl}/upload/chunk`, formData, { reportProgress: true, observe: 'events' });
   }
 
   completeUpload(uploadId: string): Observable<ApiResponse<ResourceResponse>> {
