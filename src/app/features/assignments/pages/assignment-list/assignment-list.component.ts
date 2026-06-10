98c24add643d40f357170ad89992335bda1c1f2a
vinuth <vinuthsriarampath@outlook.com>
2026-06-10 09:40:20 +0530

    feat: create AssignmentListComponent with table display and pagination for assignments

---
+++ src/app/features/assignments/pages/assignment-list/assignment-list.component.ts
@@ -0,0 +1,96 @@
+import {Component, inject, input, OnInit} from '@angular/core';
+import {DatePipe} from "@angular/common";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+  MatTableDataSource
+} from "@angular/material/table";
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
+import {MatTooltip} from "@angular/material/tooltip";
+import {Assignment} from '@features/assignments/dtos/response/assignment';
+import {ModuleService} from '@features/module/services/module/module.service';
+import {ChapterService} from '@features/chapter/services/chapter/chapter.service';
+import {AlertService} from '@core/services/alerts/alert.service';
+import {Eye, LucideAngularModule, Pencil} from 'lucide-angular';
+
+export type AssignmentListConfig =
+  | { type: 'module'; moduleId: number; chapterId?: never }
+  | { type: 'chapter'; chapterId: number; moduleId?: never };
+
+@Component({
+  selector: 'app-assignment-list',
+  imports: [
+    DatePipe,
+    LucideAngularModule,
+    MatCell,
+    MatCellDef,
+    MatColumnDef,
+    MatHeaderCell,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatPaginator,
+    MatRow,
+    MatRowDef,
+    MatTable,
+    MatTooltip,
+    MatHeaderCellDef
+  ],
+  templateUrl: './assignment-list.component.html',
+  styleUrl: './assignment-list.component.css'
+})
+export class AssignmentListComponent implements OnInit {
+  config = input.required<AssignmentListConfig>();
+
+  protected columns: string[] = ['id', 'topic', 'availableDate', 'dueDate', 'createdDate', 'actions'];
+  protected dataSource: MatTableDataSource<Assignment> = new MatTableDataSource<Assignment>();
+
+  protected pageIndex = 0;
+  protected pageSize = 10;
+  protected totalElements = 0;
+  protected readonly Eye = Eye;
+  protected readonly Pencil = Pencil;
+  private readonly moduleService: ModuleService = inject(ModuleService);
+  private readonly chapterService: ChapterService = inject(ChapterService);
+  private readonly alertService: AlertService = inject(AlertService);
+
+  ngOnInit(): void {
+    this.loadAssignments();
+  }
+
+  protected loadAssignments() {
+    const currentConfig = this.config();
+
+    const assignmentObservable$ = currentConfig.type === 'module'
+      ? this.moduleService.getAssignmentsByModule(currentConfig.moduleId,this.pageIndex,this.pageSize)
+      : this.chapterService.getAllChapterAssignmentsWithFilters(currentConfig.chapterId,this.pageIndex,this.pageSize);
+
+    assignmentObservable$.subscribe({
+      next: (res) => {
+        if (res.data) {
+          this.dataSource.data = res.data;
+          this.pageIndex = res.page ?? 0;
+          this.pageSize = res.size ?? 10;
+          this.totalElements = res.totalElements ?? 0;
+        }
+      },
+      error: (err) => {
+        this.alertService.triggerErrorAlert(err.error?.message || 'Failed to load assignments');
+      }
+    });
+  }
+
+
+  protected onPageChange($event: PageEvent) {
+    this.pageIndex = $event.pageIndex;
+    this.pageSize = $event.pageSize;
+    this.loadAssignments();
+  }
+}
