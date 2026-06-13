92485bd5e2e7f0e556b26e01fe941563d7a4576a
vinuth <vinuthsriarampath@outlook.com>
2026-06-13 07:53:25 +0530

    feat: integrate LoaderOverlayComponent in dialog and page layouts for improved loading indication

--- src/app/core/layouts/dialog-layout/dialog-layout.component.ts
+++ src/app/core/layouts/dialog-layout/dialog-layout.component.ts
@@ -2,13 +2,15 @@ import {Component, input, output} from '@angular/core';
 import {LucideAngularModule, LucideIconData, Megaphone} from "lucide-angular";
 import {ReactiveFormsModule} from "@angular/forms";
 import {NgClass} from '@angular/common';
+import {LoaderOverlayComponent} from '@shared/components/loader-overlay/loader-overlay.component';
 
 @Component({
   selector: 'app-dialog-layout',
   imports: [
     LucideAngularModule,
     ReactiveFormsModule,
-    NgClass
+    NgClass,
+    LoaderOverlayComponent
   ],
   templateUrl: './dialog-layout.component.html',
   styleUrl: './dialog-layout.component.css'
--- src/app/core/layouts/page-layout/page-layout.component.ts
+++ src/app/core/layouts/page-layout/page-layout.component.ts
@@ -1,12 +1,14 @@
 import {Component, input} from '@angular/core';
 import {LucideAngularModule} from 'lucide-angular';
 import {PageTitleComponent} from '@shared/components/page-title/page-title.component';
+import {LoaderOverlayComponent} from '@shared/components/loader-overlay/loader-overlay.component';
 
 @Component({
   selector: 'app-page-layout',
   imports: [
     LucideAngularModule,
-    PageTitleComponent
+    PageTitleComponent,
+    LoaderOverlayComponent
   ],
   templateUrl: './page-layout.component.html',
   styleUrl: './page-layout.component.css'
@@ -16,4 +18,8 @@ export class PageLayoutComponent {
   headerTitle = input.required<string>();
   headerDescription = input<string>();
   showBackButton = input<boolean>(false);
+
+  isLoading = input<boolean>(false);
+  loaderTitle = input<string>('Loading');
+  loaderDescription = input<string>('Please wait...');
 }
