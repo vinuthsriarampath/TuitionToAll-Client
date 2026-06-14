bc7d495b4211ee8d11b727d92ac0c9996ab2800c
vinuth <vinuthsriarampath@outlook.com>
2026-06-14 08:23:26 +0530

    feat: add ChapterStatCountResponse class for chapter statistics

---
+++ src/app/features/chapter/dtos/response/chapter-stats-response.ts
@@ -0,0 +1,6 @@
+export class ChapterStatCountResponse {
+  resourceCount!: number;
+  lectureRecordingCount!: number;
+  upcomingScheduleLectureCount!: number;
+  activeAssignmentsCount!: number;
+}
