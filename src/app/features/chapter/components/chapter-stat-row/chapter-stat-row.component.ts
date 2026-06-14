7df0d7fc573dabcbccf439245397e9c6c44f1e37
vinuth <vinuthsriarampath@outlook.com>
2026-06-14 08:25:45 +0530

    feat: implement chapter statistics loading in chapter stat row component

--- src/app/features/chapter/components/chapter-stat-row/chapter-stat-row.component.ts
+++ src/app/features/chapter/components/chapter-stat-row/chapter-stat-row.component.ts
@@ -1,5 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {StatCardComponent} from '@shared/ui';
+import {ChapterStatCountResponse} from '@features/chapter/dtos/response/chapter-stats-response';
+import {ChapterService} from '@features/chapter/services/chapter/chapter.service';
+import {ActivatedRoute} from '@angular/router';
+import {AlertService} from '@core/services/alerts/alert.service';
+import {ChapterDetailedResponse} from '@features/chapter/dtos/response/ChapterDetailedResponse';
 
 @Component({
   selector: 'app-chapter-stat-row',
@@ -9,6 +14,35 @@ import {StatCardComponent} from '@shared/ui';
   templateUrl: './chapter-stat-row.component.html',
   styleUrl: './chapter-stat-row.component.css'
 })
-export class ChapterStatRowComponent {
+export class ChapterStatRowComponent implements OnInit{
 
+  private chapterId!:number;
+  protected chapter!:ChapterDetailedResponse;
+  protected chapterStats!:ChapterStatCountResponse;
+
+  private readonly chapterService:ChapterService = inject(ChapterService);
+  private readonly activatedRoute:ActivatedRoute = inject(ActivatedRoute);
+  private readonly alertService = inject(AlertService);
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(params =>{
+      const chapterIdParam = params.get('chapterId') ?? '' ;
+      const chapterId = Number.parseInt(chapterIdParam);
+      if (chapterId && !Number.isNaN(chapterId)){
+        this.chapterId = chapterId;
+        this.loadChapterStats();
+      }
+    })
+  }
+
+  private loadChapterStats():void{
+    this.chapterService.getChapterStats(this.chapterId).subscribe({
+      next: (res)=>{
+        if (res.data) this.chapterStats = res.data;
+      },
+      error: err => {
+        this.alertService.triggerErrorAlert(err.error.message);
+      }
+    })
+  }
 }
