877b55b9b1dcdc2d0e1c41903040773d7cfb2da6
vinuth <vinuthsriarampath@outlook.com>
2026-07-25 16:16:32 +0530

    feat: implement batch resolver and associated data interface with tests

---
+++ src/app/features/batch/resolvers/batch.resolver.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { ResolveFn } from '@angular/router';
+
+import { batchResolver } from './batch.resolver';
+
+describe('batchResolver', () => {
+  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
+      TestBed.runInInjectionContext(() => batchResolver(...resolverParameters));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('should be created', () => {
+    expect(executeResolver).toBeTruthy();
+  });
+});
