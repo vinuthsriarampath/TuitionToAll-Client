6a2a3e55623b046b39eef67ee7e76cb12a740c52
vinuth <vinuthsriarampath@outlook.com>
2026-07-30 12:56:52 +0530

    feat: update chapter header component to enhance edit functionality and emit chapter updates

--- src/app/features/chapter/components/chapter-header/chapter-header.component.ts
+++ src/app/features/chapter/components/chapter-header/chapter-header.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, input} from '@angular/core';
+import {Component, computed, inject, input, output} from '@angular/core';
 import {ChapterBadgeComponent} from "../chapter-badge/chapter-badge.component";
 import {DatePipe} from "@angular/common";
 import {ChapterStatus} from '../../enums/ChapterStatus';
@@ -8,6 +8,9 @@ import {
   ChapterUpdateDialogComponent
 } from '@features/chapter/dialogs/chapter-update-dialog/chapter-update-dialog.component';
 import {MatDialog} from '@angular/material/dialog';
+import {ActivatedRoute} from '@angular/router';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {ChapterMapper} from '@features/chapter/mapper/chapter-mapper';
 
 @Component({
   selector: 'app-chapter-header',
@@ -27,17 +30,22 @@ export class ChapterHeaderComponent {
     createdDate = input.required<string>();
     lastModifiedDate = input.required<string>();
 
-    private readonly dialog = inject(MatDialog);
+  readonly chapterUpdated = output<void>();
 
-  protected openUpdateChapterDetailsDialog(chapter:ChapterResponse):void{
+    private readonly dialog = inject(MatDialog);
+    private readonly route = inject(ActivatedRoute);
+    chapter = this.route.snapshot.data['chapter'];
+
+
+  protected openUpdateChapterDetailsDialog():void{
     const dialogRef = this.dialog.open(ChapterUpdateDialogComponent,{
       width:'450px',
-      data: chapter
+      data: ChapterMapper.toResponse(this.chapter)
     });
 
     dialogRef.afterClosed().subscribe({
       next:(res)=>{
-        // if(res) this.loadChaptersByModule();
+        if(res) this.chapterUpdated.emit();
       }
     })
 
