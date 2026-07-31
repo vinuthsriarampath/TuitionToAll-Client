ba18a49c70252fc9851d91a3d09b3ff5df9a3789
vinuth <vinuthsriarampath@outlook.com>
2026-07-31 11:36:42 +0530

    feat: add CourseTreeNode and CourseTreeNodeFactory for course structure management

---
+++ src/app/features/course/dtos/modules/course-tree-node-factory/course-tree-node-factory.spec.ts
@@ -0,0 +1,7 @@
+import { CourseTreeNodeFactory } from './course-tree-node-factory';
+
+describe('CourseTreeNodeFactory', () => {
+  it('should create an instance', () => {
+    expect(new CourseTreeNodeFactory()).toBeTruthy();
+  });
+});
