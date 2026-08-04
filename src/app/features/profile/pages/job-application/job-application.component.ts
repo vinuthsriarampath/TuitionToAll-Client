e04c2e696f7ad1dee4fc4c7e28547add81337664
vinuth <vinuthsriarampath@outlook.com>
2026-08-04 07:50:17 +0530

    feat: update job application component to use confirmation dialog for application submission

--- src/app/features/profile/pages/job-application/job-application.component.ts
+++ src/app/features/profile/pages/job-application/job-application.component.ts
@@ -11,6 +11,8 @@ import {User} from '../../dtos/response/user';
 import {ApplicationService} from '../../../applications/services/application/application.service';
 import {AlertService} from '@core/services/alerts/alert.service';
 import {TeacherVacancyStatus} from '../../../teacher-vacancy/enums/teacher-vacancy-status';
+import {ConfirmationDialogData} from '@shared/dialogs/confirmation-dialog/confirmation-dialog.component';
+import {Check} from 'lucide-angular';
 
 @Component({
   selector: 'app-job-application',
@@ -87,13 +89,19 @@ export class JobApplicationComponent implements OnInit{
 
   protected applyForVacancy() {
     if (this.isApplied) return;
-    const message =  "Apply for This Vacancy?";
-    const description = "Once you apply, your application will be recorded and sent to the institute for review. You won’t be able to submit another application for this vacancy.";
 
     const title = "Application Submitted Successfully !";
     const informationDescription = "<div class=\"text-sm font-medium tracking-wide text-sky-200/90 mt-1 leading-relaxed break-words\"><p>Your application has been successfully submitted to the institute.</p><p>The institute may contact you via <strong>email</strong>, <strong>phone call</strong>, or <strong>direct messaging</strong> regarding the next steps.</p><div><p class=\"font-medium text-sky-50 mb-1\">Important Information:</p><ul class=\"list-disc pl-5 space-y-1\"><li>All interviews and evaluations are conducted directly by the institute.</li><li>TuitionToAll does not manage or participate in the selection process.</li><li>TuitionToAll is not responsible for any issues during or after the selection process.</li><li>If selected, you will receive an official email via TuitionToAll on behalf of the institute.</li><li>The institute may also contact you separately based on their procedures.</li><li>After selection, all responsibilities and workload are handled by the institute.</li></ul></div><p>You can track your application status from your dashboard.</p></div>";
 
-    this.alertService.triggerSuccessConfirmationAlert(message,description).subscribe({
+    const dialogData:ConfirmationDialogData = {
+      title: 'Apply for This Vacancy?',
+      message: 'Once you apply, your application will be recorded and sent to the institute for review. You won’t be able to submit another application for this vacancy.',
+      confirmText: 'Ok',
+      confirmButtonClass: 'btn-mini-primary btn-success',
+      type: 'success',
+      icon: Check
+    }
+    this.alertService.triggerSuccessConfirmationAlert(dialogData).subscribe({
       next: (res)=>{
         if(res){
           if (this.vacancy && this.currentUser.details) {
