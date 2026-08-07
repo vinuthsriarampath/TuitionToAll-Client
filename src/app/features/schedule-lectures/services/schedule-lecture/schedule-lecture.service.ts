5ff6acedc37b9e20133f23ed231976b00ccafc92
vinuth <vinuthsriarampath@outlook.com>
2026-08-07 21:33:31 +0530

    feat: add canJoinLecture method to schedule lecture service for attendance validation

--- src/app/features/schedule-lectures/services/schedule-lecture/schedule-lecture.service.ts
+++ src/app/features/schedule-lectures/services/schedule-lecture/schedule-lecture.service.ts
@@ -6,6 +6,8 @@ import {Observable} from 'rxjs';
 import {ScheduleLectureResponse} from '../../dtos/response/ScheduleLectureResponse';
 import {ApiResponse} from "@shared/utils/response/api-response";
 import {ScheduleLectureUpdateRequest} from '../../dtos/request/ScheduleLectureUpdateRequest';
+import {ScheduleLectureStatus} from '@features/schedule-lectures/enums/ScheduleLectureStatus';
+import {getDate, getTime} from '@shared/utils/helpers/date-helper';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +24,31 @@ export class ScheduleLectureService {
 
   updateScheduleLecture(scheduleLectureId: number, request: ScheduleLectureUpdateRequest): Observable<ApiResponse<ScheduleLectureResponse>> {
     return this.http.put<ApiResponse<ScheduleLectureResponse>>(`${this.baseUrl}/${scheduleLectureId}`, request);
+  }
+
+  canJoinLecture(scheduleLecture: ScheduleLectureResponse): boolean {
+
+    const currentDate = getDate(0, 0);
+    const currentTime = getTime(0, 0);
+
+    const isToday:boolean = scheduleLecture.startDate === currentDate;
+
+    const hasStarted:boolean = currentTime >= scheduleLecture.startTime;
+
+    const notEnded:boolean = currentTime <= scheduleLecture.endTime;
+
+    const validStatus:boolean = scheduleLecture.status === ScheduleLectureStatus.SCHEDULED || scheduleLecture.status === ScheduleLectureStatus.LIVE;
+
+    let validAttendance:boolean;
+
+    if (scheduleLecture.lateAttendance) {
+      validAttendance = true;
+    } else {
+      const startDateTime = new Date(`${scheduleLecture.startDate}T${scheduleLecture.startTime}`);
+      startDateTime.setMinutes(startDateTime.getMinutes() + 10);
+      const now = new Date();
+      validAttendance = now <= startDateTime;
+    }
+    return (isToday && hasStarted && notEnded && validStatus && validAttendance);
   }
 }
