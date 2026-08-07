e0683a8aa960b2d5cc2227293ff1efd3615d4d46
vinuth <vinuthsriarampath@outlook.com>
2026-08-07 22:19:41 +0530

    feat: add profile intro component with user details and edit functionality

---
+++ src/app/features/profile/components/profile-intro/profile-intro.component.ts
@@ -0,0 +1,65 @@
+import {Component, inject, input, output} from '@angular/core';
+import {CardShellComponent} from '@shared/ui';
+import {Calendar, LucideAngularModule, MapPin, Phone} from 'lucide-angular';
+import {NgIf} from '@angular/common';
+import {PhonePipePipe} from '@shared/utils/pipes/phone-pipe/phone-pipe.pipe';
+import {User} from '@features/profile/dtos/response/user';
+import {AlertService} from '@core/services/alerts/alert.service';
+import {MatDialog} from '@angular/material/dialog';
+import {
+  UpdateProfileDialogComponent
+} from '@features/profile/dialogs/update-profile-dialog/update-profile-dialog.component';
+import {UserHelper} from '@shared/utils/helpers/user-helper';
+
+@Component({
+  selector: 'app-profile-intro',
+  imports: [
+    CardShellComponent,
+    LucideAngularModule,
+    NgIf,
+    PhonePipePipe
+  ],
+  templateUrl: './profile-intro.component.html',
+  styleUrl: './profile-intro.component.css'
+})
+export class ProfileIntroComponent {
+
+  profileUser = input.required<User>();
+  currentUser = input.required<User>();
+  isSameUser = input.required<boolean>();
+  loading = input<boolean>(false);
+
+  profileUpdated = output<void>()
+
+  private readonly alertService:AlertService = inject(AlertService);
+  private readonly dialog:MatDialog = inject(MatDialog);
+
+  openProfileUpdateDialog() {
+    let userDetails:User = this.profileUser();
+
+    const dialogRef = this.dialog.open(UpdateProfileDialogComponent, {
+      maxWidth: '80vh',
+      width: '100%',
+      panelClass: 'update-profile-dialog',
+      data: {
+        userRole: structuredClone(this.profileUser().role?.role),
+        details: structuredClone(userDetails)
+      }
+    });
+
+    dialogRef.afterClosed().subscribe({
+      next: (res) => {
+        if (res) {
+          this.alertService.triggerSuccessAlert();
+          this.profileUpdated.emit(res);
+        }
+        this.alertService.triggerErrorAlert();
+      }
+    });
+  }
+
+  protected readonly MapPin = MapPin;
+  protected readonly UserHelper = UserHelper;
+  protected readonly Calendar = Calendar;
+  protected readonly Phone = Phone;
+}
