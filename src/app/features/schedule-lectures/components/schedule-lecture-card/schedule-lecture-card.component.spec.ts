0812153f7848abca660aadf1bb465ad7a44e509c
vinuth <vinuthsriarampath@outlook.com>
2026-08-07 22:05:51 +0530

    feat: add schedule lecture card component with join and edit functionality

---
+++ src/app/features/schedule-lectures/components/schedule-lecture-card/schedule-lecture-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ScheduleLectureCardComponent } from './schedule-lecture-card.component';
+
+describe('ScheduleLectureCardComponent', () => {
+  let component: ScheduleLectureCardComponent;
+  let fixture: ComponentFixture<ScheduleLectureCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ScheduleLectureCardComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ScheduleLectureCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
