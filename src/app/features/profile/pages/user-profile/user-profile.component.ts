72bee6f772f18977b9e61d8f9ed40edbc2385c61
vinuth <vinuthsriarampath@outlook.com>
2026-08-07 22:21:03 +0530

    feat: replace user profile sections with dedicated components for cover, intro, and tabs

--- src/app/features/profile/pages/user-profile/user-profile.component.ts
+++ src/app/features/profile/pages/user-profile/user-profile.component.ts
@@ -1,63 +1,38 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {NavbarComponent} from '@shared/components/navbar/navbar.component';
-import {NgIf, NgOptimizedImage, NgSwitch, NgSwitchCase, NgSwitchDefault} from '@angular/common';
 import {User} from '../../dtos/response/user';
 import {UserService} from '../../services/user/user.service';
-import {MatDialog} from '@angular/material/dialog';
-import {UpdateProfileDialogComponent} from '../../components/update-profile-dialog/update-profile-dialog.component';
-import {AlertService} from '@core/services/alerts/alert.service';
-import {Calendar, LucideAngularModule, MapPin, Pen, Phone} from 'lucide-angular';
-import {
-  UpdateUserProfilePicDialogComponent
-} from '../../components/update-user-profile-pic-dialog/update-user-profile-pic-dialog.component';
+import {LucideAngularModule} from 'lucide-angular';
 import {environment} from '@env/environment.development';
-import {
-  UpdateProfileBannerDialogComponent
-} from '../../components/update-profile-banner-dialog/update-profile-banner-dialog.component';
-import {MatTab, MatTabContent, MatTabGroup} from '@angular/material/tabs';
-import {UserPostsComponent} from '../user-posts/user-posts.component';
-import {UserCoursesComponent} from '../user-courses/user-courses.component';
-import {InstituteJobsComponent} from '../institute-jobs/institute-jobs.component';
+import {ProfileCoverComponent} from '@features/profile/components/profile-cover/profile-cover.component';
+import {ProfileIntroComponent} from '@features/profile/components/profile-intro/profile-intro.component';
+import {ProfileTabsComponent} from '@features/profile/components/profile-tabs/profile-tabs.component';
 import {CardShellComponent} from '@shared/ui';
-import {PhonePipePipe} from '@shared/utils/pipes/phone-pipe/phone-pipe.pipe';
 
 @Component({
   selector: 'app-user-profile',
   imports: [
     NavbarComponent,
-    NgSwitch,
-    NgSwitchCase,
-    NgSwitchDefault,
-    NgIf,
     LucideAngularModule,
-    NgOptimizedImage,
-    MatTabGroup,
-    MatTab,
-    MatTabContent,
-    UserPostsComponent,
-    UserCoursesComponent,
-    InstituteJobsComponent,
     CardShellComponent,
-    PhonePipePipe,
+    ProfileCoverComponent,
+    ProfileIntroComponent,
+    ProfileTabsComponent,
   ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent implements OnInit {
-  readonly Pen = Pen;
 
-  isSameUser: boolean = false;
-  currentUser: User = new User();
-  profileUser:User = new User();
-  userRole: string = '';
+  protected loading: boolean = false;
+  protected isSameUser: boolean = false;
+  protected currentUser!: User;
+  protected profileUser!:User;
 
   private readonly userService: UserService = inject(UserService);
-  private readonly alertService:AlertService = inject(AlertService);
-
-  constructor(
-    private readonly activatedRoute: ActivatedRoute,
-    private  readonly dialog:MatDialog) {}
+  private readonly activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  private readonly router:Router = inject(Router);
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(params => {
@@ -71,113 +46,28 @@ export class UserProfileComponent implements OnInit {
   }
 
   private loadUserData(userSlug: string) {
+    this.triggerLoading();
     this.userService.findUserByUserSlug(userSlug).subscribe({
       next: (res) => {
         const user: User = res.data!;
-        this.userRole = user.role?.role || '';
 
         this.profileUser = structuredClone(user);
-        this.isSameUser = this.currentUser.email === user.email;
+        this.isSameUser = this.currentUser.userSlug === user.userSlug;
+        this.triggerLoading();
       },
-      error() {
-        window.location.replace('/app');
+      error: () =>{
+        void this.router.navigate(['app']);
       }
     });
   }
 
-  openProfilePicUpdateDialog() {
-    let userDetails:User = this.profileUser;
-
-    const dialogRef = this.dialog.open(UpdateUserProfilePicDialogComponent, {
-      maxWidth: '80vh',
-      width: '100%',
-      panelClass: 'update-profile-dp-dialog',
-      data: {
-        userRole: structuredClone(this.userRole),
-        details: structuredClone(userDetails)
-      }
-    });
-
-    const alert:string = "Dp updated successfully";
-
-    dialogRef.afterClosed().subscribe({
-      next: (res) => {
-        if (res) {
-          this.alertService.triggerSuccessAlert(alert);
-          this.profileUser = structuredClone(res);
-          if(this.isSameUser) this.userService.setCurrentUser(this.profileUser);
-          return;
-        }
-        this.alertService.triggerErrorAlert();
-      }
-    });
+  protected updateProfileDetails = (res: any) => {
+    this.profileUser = structuredClone(res);
+    if(this.isSameUser) this.userService.setCurrentUser(this.profileUser);
   }
 
-  openProfileUpdateDialog() {
-    let userDetails:User = this.profileUser;
-
-    const dialogRef = this.dialog.open(UpdateProfileDialogComponent, {
-      maxWidth: '80vh',
-      width: '100%',
-      panelClass: 'update-profile-dialog',
-      data: {
-        userRole: structuredClone(this.userRole),
-        details: structuredClone(userDetails)
-      }
-    });
-
-    dialogRef.afterClosed().subscribe({
-      next: (res) => {
-        if (res) {
-          this.alertService.triggerSuccessAlert();
-          this.profileUser = structuredClone(res);
-          if(this.isSameUser) this.userService.setCurrentUser(this.profileUser);
-          return;
-        }
-        this.alertService.triggerErrorAlert();
-      }
-    });
+  private triggerLoading() {
+    this.loading = !this.loading;
   }
-
-  openProfileBannerUpdateDialog() {
-    let userDetails:User = this.profileUser;
-
-    const dialogRef = this.dialog.open(UpdateProfileBannerDialogComponent, {
-      maxWidth: '100vh',
-      width: '100%',
-      panelClass: 'update-profile-banner-dialog',
-      data: {
-        userRole: structuredClone(this.userRole),
-        details: structuredClone(userDetails)
-      }
-    });
-
-    const alert:string = "Banner updated successfully!";
-
-    dialogRef.afterClosed().subscribe({
-      next: (res) => {
-        if (res) {
-          this.alertService.triggerSuccessAlert(alert);
-          this.profileUser = structuredClone(res);
-          if(this.isSameUser) this.userService.setCurrentUser(this.profileUser);
-          return;
-        }
-        this.alertService.triggerErrorAlert();
-      }
-    });
-  }
-  tabLoadTimes: Date[] = [];
-
-  getTimeLoaded(index: number) {
-    if (!this.tabLoadTimes[index]) {
-      this.tabLoadTimes[index] = new Date();
-    }
-
-    return this.tabLoadTimes[index];
-  }
-
   protected readonly environment = environment;
-  protected readonly Phone = Phone;
-  protected readonly Calendar = Calendar;
-  protected readonly MapPin = MapPin;
 }
