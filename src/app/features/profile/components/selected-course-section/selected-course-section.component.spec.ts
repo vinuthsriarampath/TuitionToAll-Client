92adf4350628bf1553814ad1acaedcd9738775a8
vinuth <vinuthsriarampath@outlook.com>
2026-08-10 15:01:46 +0530

    feat: add SelectedCourseSection component with course details display

---
+++ src/app/features/profile/components/selected-course-section/selected-course-section.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SelectedCourseSectionComponent } from './selected-course-section.component';
+
+describe('SelectedCourseSectionComponent', () => {
+  let component: SelectedCourseSectionComponent;
+  let fixture: ComponentFixture<SelectedCourseSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SelectedCourseSectionComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SelectedCourseSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
