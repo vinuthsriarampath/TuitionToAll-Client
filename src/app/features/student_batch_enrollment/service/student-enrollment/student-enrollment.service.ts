e22d66c297de292821b9a13e8cded6ae07953e4d
vinuth <vinuthsriarampath@outlook.com>
2026-08-13 08:32:37 +0530

    feat: add checkEnrollmentEligibility method to verify student enrollment eligibility

--- src/app/features/student_batch_enrollment/service/student-enrollment/student-enrollment.service.ts
+++ src/app/features/student_batch_enrollment/service/student-enrollment/student-enrollment.service.ts
@@ -1,8 +1,15 @@
 import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {environment} from '@env/environment.development';
 import {Observable} from 'rxjs';
 import {EnrollmentRequest} from '@features/student_batch_enrollment/dto/request/enrollment-request/enrollment-request';
+import {
+  EnrollmentEligibilityCheckRequest
+} from '@features/student_batch_enrollment/dto/request/enrollment-eligibility-check-request/enrollment-eligibility-check-request';
+import {
+  EnrollmentEligibilityResponse
+} from '@features/student_batch_enrollment/dto/response/enrollment-eligibility-response/enrollment-eligibility-response';
+import {ApiResponse} from '@shared/utils/response/api-response';
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +22,11 @@ export class StudentEnrollmentService {
     return this.http.post<Blob>(`${this.baseUrl}`, request,{responseType: 'blob' as 'json'});
   }
 
+  checkEnrollmentEligibility(request: EnrollmentEligibilityCheckRequest): Observable<ApiResponse<EnrollmentEligibilityResponse>> {
+    let params = new HttpParams()
+      .set('courseId', request.courseId.toString())
+      .set('batchId', request.batchId.toString());
+
+    return this.http.get<ApiResponse<EnrollmentEligibilityResponse>>(`${this.baseUrl}/eligibility-check`, { params } );
+  }
 }
