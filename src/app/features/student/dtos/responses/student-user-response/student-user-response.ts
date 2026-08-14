ba2d06b72b39a6b33e7a86a1cdae0bd7ba56f917
vinuth <vinuthsriarampath@outlook.com>
2026-08-14 23:26:46 +0530

    feat: add StudentUserResponse class with required properties and test suite

---
+++ src/app/features/student/dtos/responses/student-user-response/student-user-response.ts
@@ -0,0 +1,12 @@
+export class StudentUserResponse {
+  studentId!: number;
+  firstName!: string;
+  lastName!: string;
+  dob!: string;
+
+  userId!: number;
+  email!: string;
+  contact!: string;
+  dp!: string;
+  address!: string;
+}
