5ef0e29f94af1e9b2c8cee5b633e5323074f7cd6
vinuth <vinuthsriarampath@outlook.com>
2026-08-16 20:15:51 +0530

    feat: add ReviewCreateCard component for submitting reviews with form validation

---
+++ src/app/features/review/components/review-create-card/review-create-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReviewCreateCardComponent } from './review-create-card.component';
+
+describe('ReviewCreateCardComponent', () => {
+  let component: ReviewCreateCardComponent;
+  let fixture: ComponentFixture<ReviewCreateCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReviewCreateCardComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ReviewCreateCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
