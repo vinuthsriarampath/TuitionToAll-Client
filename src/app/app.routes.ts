29a927e5f94eb64e4e01fbe0a515dbd9218c22be
vinuth <vinuthsriarampath@outlook.com>
2026-08-17 11:27:13 +0530

    refactor: update import paths and rename institute-dashboard component files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -24,7 +24,7 @@ import {
 import {PasswordResetPageComponent} from '@features/auth/pages/password-reset-page/password-reset-page.component';
 import {AppComponent} from '@core/layouts/app/app.component';
 import {FeedComponent} from '@features/feed/pages/feed.component';
-import {InstituteDashboardComponent} from '@features/dashboards/institute-dashboard/institute-dashboard.component';
+import {InstituteDashboardComponent} from '@features/dashboards/pages/institute-dashboard/institute-dashboard.component';
 import {instituteRoleGuard} from '@core/guards/institute-role-guard/institute-role.guard';
 import {DashboardComponent} from '@features/institute/pages/dashboard/dashboard.component';
 import {
