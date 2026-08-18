3751bbb54f08f85bd65cd4d3e70106811cb152a3
vinuth <vinuthsriarampath@outlook.com>
2026-08-18 22:23:02 +0530

    feat: update page layout component to conditionally render title and adjust padding

--- src/app/core/layouts/page-layout/page-layout.component.ts
+++ src/app/core/layouts/page-layout/page-layout.component.ts
@@ -2,20 +2,22 @@ import {Component, input} from '@angular/core';
 import {LucideAngularModule} from 'lucide-angular';
 import {PageTitleComponent} from '@shared/components/page-title/page-title.component';
 import {LoaderOverlayComponent} from '@shared/components/loader-overlay/loader-overlay.component';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-page-layout',
   imports: [
     LucideAngularModule,
     PageTitleComponent,
-    LoaderOverlayComponent
+    LoaderOverlayComponent,
+    NgClass
   ],
   templateUrl: './page-layout.component.html',
   styleUrl: './page-layout.component.css'
 })
 export class PageLayoutComponent {
 
-  headerTitle = input.required<string>();
+  headerTitle = input<string>();
   headerDescription = input<string>();
   showBackButton = input<boolean>(false);
 
