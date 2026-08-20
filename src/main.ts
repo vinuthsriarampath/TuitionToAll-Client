08bca6f4212c115b91bec05e91ac6d6a34df3031
vinuth <vinuthsriarampath@outlook.com>
2026-08-20 08:51:45 +0530

    feat: add ng2-charts and chart.js for enhanced data visualization

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,14 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {appConfig} from './app/app.config';
 import {AppComponent} from './app/app.component';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 
 setTimeout(() => {
   bootstrapApplication(AppComponent, {
     ...appConfig,
     providers: [
-      ...(appConfig.providers || []),
+      ...(appConfig.providers || []), provideCharts(withDefaultRegisterables()),
     ]
   })
   .catch(err => console.error(err));
