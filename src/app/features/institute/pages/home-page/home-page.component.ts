11e360e4e7f115c29392850ab21cea1f09ee4af7
vinuth <vinuthsriarampath@outlook.com>
2026-08-23 22:07:47 +0530

    feat: implement loading state and fetch bootstrap data for home page

--- src/app/features/institute/pages/home-page/home-page.component.ts
+++ src/app/features/institute/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {PageLayoutComponent} from '@core/layouts';
 import {KpiCardComponent} from '@features/institute/components/kpi-card/kpi-card.component';
 import {
@@ -26,6 +26,9 @@ import {
   RecentActivityComponentComponent
 } from '@features/institute/components/recent-activity-component/recent-activity-component.component';
 import {RouterLink} from '@angular/router';
+import {InstituteService} from '@features/institute/services/institute/institute.service';
+import {AlertService} from '@core/services/alerts/alert.service';
+import {InstituteBootstrapResponse} from '@features/institute/dtos/response/institute-bootstrap-response';
 
 @Component({
   selector: 'app-home-page',
@@ -43,7 +46,42 @@ import {RouterLink} from '@angular/router';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
+
+  protected bootstrapData = new InstituteBootstrapResponse();
+  protected isLoading:boolean = false;
+
+
+  private readonly instituteService = inject(InstituteService);
+  private readonly alertService = inject(AlertService);
+
+  ngOnInit(): void {
+      this.fetchBootstrapData();
+  }
+
+  private fetchBootstrapData():void{
+    this.triggerLoading();
+    this.instituteService.getInstituteBootstrapData().subscribe({
+      next: (res) => {
+        if(res.data){
+          this.bootstrapData = res.data;
+        }
+        this.triggerLoading();
+      },
+      error: () => {
+        this.triggerLoading();
+        this.alertService.triggerErrorAlert('Failed to load dashboard data. Please try again later.');
+      }
+    })
+  }
+
+  protected refreshDashboard(): void {
+    this.fetchBootstrapData();
+  }
+
+  private triggerLoading():void{
+    this.isLoading = !this.isLoading;
+  }
 
   protected readonly Users = Users;
   protected readonly BookOpen = BookOpen;
