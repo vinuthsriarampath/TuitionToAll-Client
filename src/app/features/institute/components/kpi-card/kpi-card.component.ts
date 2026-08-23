a2b64fd4b2ec370a325bab159ba80c94de4b192d
vinuth <vinuthsriarampath@outlook.com>
2026-08-23 22:07:21 +0530

    feat: refactor KPI card component to use data object and improve change value display

--- src/app/features/institute/components/kpi-card/kpi-card.component.ts
+++ src/app/features/institute/components/kpi-card/kpi-card.component.ts
@@ -1,9 +1,11 @@
-import {Component, input, OnInit} from '@angular/core';
+import {Component, computed, effect, input} from '@angular/core';
 import {ChartConfiguration, ChartType} from 'chart.js';
 import {BaseChartDirective} from 'ng2-charts';
 import {ArrowDownRight, ArrowUpRight, LucideAngularModule, LucideIconData} from 'lucide-angular';
-import {NgClass} from '@angular/common';
+import {DecimalPipe, NgClass} from '@angular/common';
 import {CardShellComponent} from '@shared/ui';
+import {DashboardKpi} from '@shared/utils/response/dashboard-kpi';
+import {ChangeValueType} from '@shared/utils/enums/change-value-type';
 
 @Component({
   selector: 'app-kpi-card',
@@ -11,22 +13,57 @@ import {CardShellComponent} from '@shared/ui';
     BaseChartDirective,
     LucideAngularModule,
     NgClass,
-    CardShellComponent
+    CardShellComponent,
+    DecimalPipe
   ],
   templateUrl: './kpi-card.component.html',
   styleUrl: './kpi-card.component.css'
 })
-export class KpiCardComponent implements OnInit{
+export class KpiCardComponent{
   icon = input.required<LucideIconData>();
   title = input.required<string>();
-  value = input.required<string>();
-  changeValue = input.required<string>();
-  changeLabel = input.required<string>();
-  sparklineData = input<number[]>([]);
-  isPositive = input<boolean>(true);
+  data = input.required<DashboardKpi>();
+
+  readonly isChangeValuePositive = computed(() =>
+    this.data().changeValue >= 0
+  );
+
+  readonly isPercentageChange = computed(() =>
+    this.data().changeValueType === ChangeValueType.PERCENTAGE
+  );
 
   protected sparklineType:ChartType = 'line';
-  protected sparklineConfig!: ChartConfiguration['data'];
+
+  readonly sparklineData = computed<ChartConfiguration<'line'>['data']>(() => {
+
+    const kpi = this.data();
+
+    const strokeColor = kpi.changeValue >= 0 ? '#10B981' : '#EF4444';
+
+    const hasTrendData = kpi.trend.length > 0;
+
+    const labels = hasTrendData ? kpi.trend.map(point => point.date) : ['', '', '', '', '', ''];
+
+    const values = hasTrendData ? kpi.trend.map(point => point.value) : [0, 0, 0, 0, 0, 0];
+
+    return {
+      labels,
+      datasets: [
+        {
+          data: values,
+          borderColor: strokeColor,
+          backgroundColor: 'transparent',
+          borderWidth: 2,
+          fill: false,
+          tension: 0.4,
+          pointRadius: 0,
+          pointHoverRadius: 0
+        }
+      ]
+    };
+  });
+
+
   protected sparklineOptions:ChartConfiguration['options'] = {
     responsive: true,
     maintainAspectRatio: false,
@@ -36,28 +73,15 @@ export class KpiCardComponent implements OnInit{
     },
     scales: {
       x: { display:false },
-      y: { display:false }
+      y: { grid:{display: false},ticks: {
+          color: '#FFFFFF'
+        },  }
     },
     plugins: {
       legend: { display: false },
       tooltip: { enabled: false }
     }
-  }
-
-  ngOnInit(): void {
-    const strokeColor = this.isPositive() ? '#10B981' : '#EF4444';
-    this.sparklineConfig = {
-      labels: this.sparklineData().map(() => ''),
-      datasets: [
-        {
-          data: this.sparklineData(),
-          borderColor: strokeColor,
-          backgroundColor: 'transparent',
-          fill: false
-        }
-      ]
-    };
-  }
+  };
 
   protected readonly ArrowUpRight = ArrowUpRight;
   protected readonly ArrowDownRight = ArrowDownRight;
