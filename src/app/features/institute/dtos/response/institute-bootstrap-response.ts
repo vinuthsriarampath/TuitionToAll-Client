a39c18b897ddcef214fc766f85c47136978edecd
vinuth <vinuthsriarampath@outlook.com>
2026-08-24 22:10:06 +0530

    feat: add CoursePerformanceResponse class and integrate it into InstituteBootstrapResponse

--- src/app/features/institute/dtos/response/institute-bootstrap-response.ts
+++ src/app/features/institute/dtos/response/institute-bootstrap-response.ts
@@ -1,5 +1,7 @@
 import {InstituteDashboardKpiStats} from '@features/institute/dtos/response/institute-dashboard-kpi-stats';
+import {CoursePerformanceResponse} from '@features/course/dtos/response/course-performance-response';
 
 export class InstituteBootstrapResponse {
   kpiStats!: InstituteDashboardKpiStats;
+  coursePerformance!: CoursePerformanceResponse[];
 }
