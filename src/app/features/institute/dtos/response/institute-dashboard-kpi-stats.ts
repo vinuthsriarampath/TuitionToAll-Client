ed8be931b8de253a9e3131a104b11b8ea1034918
vinuth <vinuthsriarampath@outlook.com>
2026-08-24 12:47:31 +0530

    feat: rename activeCourses to publishedCourses in KPI stats

--- src/app/features/institute/dtos/response/institute-dashboard-kpi-stats.ts
+++ src/app/features/institute/dtos/response/institute-dashboard-kpi-stats.ts
@@ -3,7 +3,7 @@ import {DashboardKpi} from '@shared/utils/response/dashboard-kpi';
 export class InstituteDashboardKpiStats {
   activeStudents!: DashboardKpi;
   ongoingBatches!: DashboardKpi;
-  activeCourses!: DashboardKpi;
+  publishedCourses!: DashboardKpi;
   activeTeachers!: DashboardKpi;
   revenue!: DashboardKpi;
 }
