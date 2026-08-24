1a1570d7213a2b7f6a3693a4fcde0caf8cc4cfab
vinuth <vinuthsriarampath@outlook.com>
2026-08-24 22:12:16 +0530

    feat: update performance widget to use CoursePerformanceResponse and improve course display

--- src/app/features/institute/components/performance-widget/performance-widget.component.ts
+++ src/app/features/institute/components/performance-widget/performance-widget.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
 import {ArrowRight, LucideAngularModule, Star} from 'lucide-angular';
 import {DecimalPipe} from '@angular/common';
 import {CardShellComponent} from '@shared/ui';
+import {CoursePerformanceResponse} from '@features/course/dtos/response/course-performance-response';
+import {NoContentComponent} from '@shared/components/no-content/no-content.component';
+import {RouterLink} from '@angular/router';
 export interface CoursePerformance {
   rank: string;
   title: string;
@@ -16,18 +19,15 @@ export interface CoursePerformance {
   imports: [
     LucideAngularModule,
     DecimalPipe,
-    CardShellComponent
+    CardShellComponent,
+    NoContentComponent,
+    RouterLink
   ],
   templateUrl: './performance-widget.component.html',
   styleUrl: './performance-widget.component.css'
 })
 export class PerformanceWidgetComponent {
-  courses: CoursePerformance[] = [
-    { rank: '01', title: 'Java Programming', studentsCount: 324, activeBatchesCount: 6, rating: 4.8, progressPercentage: 100 },
-    { rank: '02', title: 'Spring Boot', studentsCount: 286, activeBatchesCount: 5, rating: 4.7, progressPercentage: 85 },
-    { rank: '03', title: 'Angular', studentsCount: 241, activeBatchesCount: 4, rating: 4.6, progressPercentage: 70 },
-    { rank: '04', title: 'Database Management', studentsCount: 198, activeBatchesCount: 3, rating: 4.5, progressPercentage: 58 }
-  ];
+  courses = input.required<CoursePerformanceResponse[]>();
   protected readonly ArrowRight = ArrowRight;
   protected readonly Star = Star;
 }
