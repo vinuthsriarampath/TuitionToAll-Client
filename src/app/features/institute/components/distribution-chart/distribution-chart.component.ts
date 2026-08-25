fe47f24e06321e54da537b045c9c0a6c813e3bb8
vinuth <vinuthsriarampath@outlook.com>
2026-08-25 12:55:19 +0530

    feat: update distribution chart to reflect enrollment status and improve data binding

--- src/app/features/institute/components/distribution-chart/distribution-chart.component.ts
+++ src/app/features/institute/components/distribution-chart/distribution-chart.component.ts
@@ -1,33 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 import {ChartConfiguration, ChartType} from 'chart.js';
 import {BaseChartDirective} from 'ng2-charts';
 import {CardShellComponent} from '@shared/ui';
+import {
+  EnrollmentDistributionResponse
+} from '@features/student-batch-enrollment/dtos/responses/enrollment-distribution-response';
+import {DecimalPipe} from '@angular/common';
 
 @Component({
   selector: 'app-distribution-chart',
   imports: [
     BaseChartDirective,
-    CardShellComponent
+    CardShellComponent,
+    DecimalPipe
   ],
   templateUrl: './distribution-chart.component.html',
   styleUrl: './distribution-chart.component.css'
 })
 export class DistributionChartComponent {
-  // public doughnutChartType: ChartType = 'doughnut';
+  data = input.required<EnrollmentDistributionResponse>();
 
-  public doughnutChartData: ChartConfiguration<'doughnut'>['data'] = {
-    labels: ['Active', 'Completed', 'Pending'],
-    datasets: [
-      {
-        data: [899, 225, 75],
-        backgroundColor: ['#0F766E', '#6EE7B7', '#F59E0B'],
-        borderWidth: 0,
-        hoverOffset: 4
-      }
-    ]
-  };
+  protected doughnutChartData = computed<ChartConfiguration<'doughnut'>['data']>(() => {
+    const distribution = this.data();
+    return{
+      labels: ['Active', 'Completed', 'Suspended'],
+      datasets: [
+        {
+          data: [distribution.activeEnrollments, distribution.completedEnrollments, distribution.suspendedEnrollments],
+          backgroundColor: ['#0F766E', '#6EE7B7', '#F59E0B'],
+          borderWidth: 0,
+          hoverOffset: 4
+        }
+      ]
+    }
+  });
 
-  public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
+  protected doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
     responsive: true,
     maintainAspectRatio: false,
     cutout: '75%',
