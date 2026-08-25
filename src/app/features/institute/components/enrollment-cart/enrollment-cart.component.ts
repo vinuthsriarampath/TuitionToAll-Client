696be53fb4b383a7e4157590e2176c82d7e89ceb
vinuth <vinuthsriarampath@outlook.com>
2026-08-25 17:59:38 +0530

    feat: enhance enrollment chart data binding and update display options

--- src/app/features/institute/components/enrollment-cart/enrollment-cart.component.ts
+++ src/app/features/institute/components/enrollment-cart/enrollment-cart.component.ts
@@ -1,43 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 import {ChartConfiguration, ChartType} from 'chart.js';
 import {BaseChartDirective} from 'ng2-charts';
 import {CardShellComponent} from '@shared/ui';
+import {OverallEnrollmentResponse} from '@features/student-batch-enrollment/dtos/responses/overall-enrollment-response';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-enrollment-cart',
   imports: [
     BaseChartDirective,
-    CardShellComponent
+    CardShellComponent,
+    DatePipe
   ],
   templateUrl: './enrollment-cart.component.html',
   styleUrl: './enrollment-cart.component.css'
 })
 export class EnrollmentCartComponent {
+  data = input.required<OverallEnrollmentResponse>();
+
   public lineChartType: ChartType = 'line';
 
-  public lineChartData: ChartConfiguration['data'] = {
-    labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-    datasets: [
-      {
-        data: [500, 600, 650, 800, 850, 940],
-        borderColor: '#0F766E',
-        backgroundColor: (context:any) => {
-          const ctx = context.chart.ctx;
-          const gradient = ctx.createLinearGradient(0, 0, 0, 250);
-          gradient.addColorStop(0, 'rgb(23 175 164 / 0.39)');
-          gradient.addColorStop(1, 'rgba(15, 118, 110, 0.0)');
-          return gradient;
-        },
-        fill: true,
-        tension: 0.4,
-        pointBackgroundColor: '#0F766E',
-        pointBorderColor: '#FFFFFF',
-        pointBorderWidth: 2,
-        pointRadius: 5,
-        pointHoverRadius: 7
-      }
-    ]
-  };
+  public lineChartData = computed<ChartConfiguration['data']>( () => {
+    const enrollmentData = this.data();
+
+    const labels:string[] = enrollmentData.trendPoints.map(point =>
+      new Intl.DateTimeFormat('en-US', { year: '2-digit', month: 'short'}).format(new Date(point.date))
+    );
+    const values:number[] = enrollmentData.trendPoints.map(point => point.value);
+    return {
+      labels: labels,
+      datasets: [
+        {
+          data: values,
+          borderColor: '#0F766E',
+          backgroundColor: (context:any) => {
+            const ctx = context.chart.ctx;
+            const gradient = ctx.createLinearGradient(0, 0, 0, 250);
+            gradient.addColorStop(0, 'rgb(23 175 164 / 0.39)');
+            gradient.addColorStop(1, 'rgba(15, 118, 110, 0.0)');
+            return gradient;
+          },
+          fill: true,
+          tension: 0.4,
+          pointBackgroundColor: '#0F766E',
+          pointBorderColor: '#FFFFFF',
+          pointBorderWidth: 2,
+          pointRadius: 5,
+          pointHoverRadius: 7
+        }
+      ]
+    }
+  });
 
   public lineChartOptions: ChartConfiguration['options'] = {
     responsive: true,
@@ -48,7 +61,8 @@ export class EnrollmentCartComponent {
     scales: {
       x: {
         grid: {
-          display: false
+          display: true,
+          color: '#0b4064'
         },
         ticks: {
           color: '#FFFFFF'
