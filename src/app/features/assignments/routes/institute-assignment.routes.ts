492f33b228bb62b31d59e1f3d27ee7ed5dd2c9dd
vinuth <vinuthsriarampath@outlook.com>
2026-09-02 22:08:43 +0530

    feat: rename institute-chapter-assignment.routes to institute-assignment.routes and update route titles

--- src/app/features/assignments/routes/institute-assignment.routes.ts
+++ src/app/features/assignments/routes/institute-assignment.routes.ts
@@ -2,10 +2,11 @@ import {Routes} from '@angular/router';
 import {assignmentResolver} from '@features/assignments/resolvers/assignment.resolver';
 import {AssignmentResolverData} from '@features/assignments/resolvers/assignment-resolver-data';
 
-export const INSTITUTE_CHAPTER_ASSIGNMENTS_ROUTES: Routes = [
+export const INSTITUTE_ASSIGNMENT_ROUTES: Routes = [
   {
     path: 'create',
     data:{breadcrumb: 'Create'},
+    title: 'Create Assignment',
     loadComponent: () => import('@features/assignments/pages/assignment-create/assignment-create.component').then(m => m.AssignmentCreateComponent)
   },
   {
@@ -16,11 +17,13 @@ export const INSTITUTE_CHAPTER_ASSIGNMENTS_ROUTES: Routes = [
       {
         path: 'update',
         data:{breadcrumb: "Update"},
+        title: (route) => route.parent?.data['assignment'].topic + ' Update',
         loadComponent: () => import('@features/assignments/pages/assignment-update/assignment-update.component').then(m => m.AssignmentUpdateComponent)
       },
       {
         path:'view',
         data:{breadcrumb: "View"},
+        title: (route) => route.parent?.data['assignment'].topic,
         loadComponent: () => import('@features/assignments/pages/assignment-view/assignment-view.component').then(m => m.AssignmentViewComponent)
       }
     ]
