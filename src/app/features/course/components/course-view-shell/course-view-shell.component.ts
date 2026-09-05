86fe2aee59067b05e83c42ce30519979a38468f7
vinuth <vinuthsriarampath@outlook.com>
2026-09-05 18:56:17 +0530

    feat: remove CourseViewComponent and related files, add CourseViewShellComponent for improved structure

---
+++ src/app/features/course/components/course-view-shell/course-view-shell.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-view-shell',
+  imports: [],
+  templateUrl: './course-view-shell.component.html',
+  styleUrl: './course-view-shell.component.css'
+})
+export class CourseViewShellComponent {
+
+}
