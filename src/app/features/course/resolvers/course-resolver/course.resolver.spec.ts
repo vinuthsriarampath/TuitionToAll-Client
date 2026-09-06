c597f49538b6c20315f1ac0945e989f22cb26d02
vinuth <vinuthsriarampath@outlook.com>
2026-09-06 19:36:17 +0530

    refactor: rename course resolver files and update resolver return type to Course

--- src/app/features/course/resolvers/course-resolver/course.resolver.spec.ts
+++ src/app/features/course/resolvers/course-resolver/course.resolver.spec.ts
@@ -2,9 +2,10 @@ import {TestBed} from '@angular/core/testing';
 import {ResolveFn} from '@angular/router';
 
 import {courseResolver} from './course.resolver';
+import {Course} from '@features/course/dtos/response/course';
 
 describe('courseResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) =>
+  const executeResolver: ResolveFn<Course> = (...resolverParameters) =>
       TestBed.runInInjectionContext(() => courseResolver(...resolverParameters));
 
   beforeEach(() => {
