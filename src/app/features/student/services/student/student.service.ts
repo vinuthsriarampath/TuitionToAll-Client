755815d6bd83e7687e5878266b198aeb8d3407fd
vinuth <vinuthsriarampath@outlook.com>
2026-09-07 10:54:35 +0530

    feat: add getStudentCourse method to retrieve course details for a student

--- src/app/features/student/services/student/student.service.ts
+++ src/app/features/student/services/student/student.service.ts
@@ -7,6 +7,7 @@ import {Student} from '@features/student/dtos/responses/student';
 import {Observable} from 'rxjs';
 import {StudentLearningResponse} from '@features/student/dtos/responses/student-learning-response';
 import {EnrollmentHistoryResponse} from '@features/student-batch-enrollment/dtos/responses/enrollment-history-response';
+import {StudentCourseViewResponse} from '@features/course/dtos/response/student-course-view-response';
 
 @Injectable({
   providedIn: 'root'
@@ -29,5 +30,9 @@ export class StudentService {
 
   getEnrollmentHistory(courseId: number): Observable<ApiResponse<EnrollmentHistoryResponse>> {
     return this.http.get<ApiResponse<EnrollmentHistoryResponse>>(`${this.baseUrl}/me/learning/courses/${courseId}/enrollment-history`);
+  }
+
+  getStudentCourse(courseId: number, batchId:number):Observable<ApiResponse<StudentCourseViewResponse>> {
+    return this.http.get<ApiResponse<StudentCourseViewResponse>>(`${this.baseUrl}/me/learning/courses/${courseId}/batches/${batchId}`);
   }
 }
