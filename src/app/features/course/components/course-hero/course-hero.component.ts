2736de4d29f9a6f02454e4a5208424923abe4193
vinuth <vinuthsriarampath@outlook.com>
2026-09-07 10:55:39 +0530

    feat: refactor course hero component to use specific input properties for course details

--- src/app/features/course/components/course-hero/course-hero.component.ts
+++ src/app/features/course/components/course-hero/course-hero.component.ts
@@ -3,6 +3,10 @@ import {Course} from "@features/course/dtos/response/course";
 import {CurrencyPipe, NgOptimizedImage} from '@angular/common';
 import {environment} from '@env/environment.development';
 import {BadgeComponent} from '@shared/ui';
+import {CourseCategory} from '@features/course/enums/course-category';
+import {CourseLevel} from '@features/course/enums/course-level';
+import {CourseLanguage} from '@features/course/enums/course-language';
+import {CourseMode} from '@features/course/enums/course-mode';
 
 @Component({
   selector: 'app-course-hero',
@@ -16,7 +20,15 @@ import {BadgeComponent} from '@shared/ui';
 })
 export class CourseHeroComponent {
 
-  course = input.required<Course>();
+  courseTitle = input.required<string>();
+  courseThumbnail = input.required<string | undefined>();
+  coursePrice = input<number>(0);
+  courseCategory = input.required<CourseCategory>();
+  courseLanguage = input.required<CourseLanguage>();
+  courseLevel = input.required<CourseLevel>();
+  courseMode = input.required<CourseMode>();
+  courseDuration = input.required<number>();
+
   courseLoading = input<boolean>(false);
   showPrice = input<boolean>(false);
 
