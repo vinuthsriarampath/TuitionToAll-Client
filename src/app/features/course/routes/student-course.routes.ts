61a77bb4f92d437f6120112fe8f66a25e938b293
vinuth <vinuthsriarampath@outlook.com>
2026-09-07 11:08:09 +0530

    feat: add route for course batches with student course resolver

--- src/app/features/course/routes/student-course.routes.ts
+++ src/app/features/course/routes/student-course.routes.ts
@@ -1,4 +1,5 @@
 import {Routes} from '@angular/router';
+import {studentCourseResolver} from '@features/course/resolvers/student-course-resolver/student-course.resolver';
 
 export const STUDENT_COURSE_ROUTES: Routes = [
   {
@@ -6,5 +7,14 @@ export const STUDENT_COURSE_ROUTES: Routes = [
     title: 'My Learnings',
     data: { breadcrumb: 'My Learnings' },
     loadComponent: () => import('@features/student/pages/student-learnings/student-learnings.component').then(m => m.StudentLearningsComponent)
+  },
+  {
+    path: 'courses/:courseId/batches/:batchId',
+    title: 'Course',
+    data: { breadcrumb: 'Course' },
+    resolve: {
+      course: studentCourseResolver
+    },
+    loadComponent: () => import('@features/course/pages/student-course-view/student-course-view.component').then(m => m.StudentCourseViewComponent)
   }
 ];
