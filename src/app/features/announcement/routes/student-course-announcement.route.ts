f72e6dd7637491c37b978324b5aa6a1ef12c78c9
vinuth <vinuthsriarampath@outlook.com>
2026-09-12 13:48:22 +0530

    feat: add routing for student course announcements with dynamic breadcrumb

---
+++ src/app/features/announcement/routes/student-course-announcement.route.ts
@@ -0,0 +1,21 @@
+import {Routes} from '@angular/router';
+import {announcementResolver} from '@features/announcement/resolvers/announcement.resolver';
+import {AnnouncementResolverData} from '@features/announcement/resolvers/announcement-resolver-data';
+
+export const STUDENT_COURSE_ROUTES: Routes = [
+  {
+    path: '',
+    data: {
+      breadcrumb: null
+    },
+    loadComponent: () => import('@features/announcement/pages/course-announcement-view/course-announcement-view.component').then(m => m.CourseAnnouncementViewComponent)
+  },
+  {
+    path: ':announcementId',
+    resolve: announcementResolver,
+    data: {
+      breadcrumb: (data:AnnouncementResolverData) => data.announcement.title
+    },
+    loadComponent: () => import('@features/announcement/pages/view-announcement/view-announcement.component').then(m => m.ViewAnnouncementComponent)
+  }
+]
