1c732eab48373f56f9342cc1ff31704e9b4eb645
vinuth <vinuthsriarampath@outlook.com>
2026-09-12 19:19:37 +0530

    feat: add conditional rendering for chapter and module edit buttons based on permissions

--- src/app/features/module/pages/module-view/module-view.component.ts
+++ src/app/features/module/pages/module-view/module-view.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, input, OnInit} from '@angular/core';
 import {ActivatedRoute, RouterLink} from '@angular/router';
 import {ModuleService} from '../../services/module/module.service';
 import {AlertService} from '@core/services/alerts/alert.service';
@@ -37,6 +37,9 @@ import {PageLayoutComponent} from '@core/layouts';
   styleUrl: './module-view.component.css'
 })
 export class ModuleViewComponent implements OnInit{
+  canEditModule = input<boolean>(false);
+  canAddChapter = input<boolean>(false);
+  canAddModuleAssignment = input<boolean>(false);
 
   protected module!:ModuleDetailedResponse;
   protected chapterRefreshTrigger:number = 0;
@@ -55,6 +58,8 @@ export class ModuleViewComponent implements OnInit{
       this.moduleId = Number.parseInt(moduleIdParam);
       if(this.moduleId && !Number.isNaN(this.moduleId)){
         this.fetchModuleDetails(this.moduleId);
+        console.log(this.canEditModule())
+        console.log(this.canAddChapter())
       }else{
         this.alertService.triggerErrorAlert("Invalid module id passed via route parameters");
       }
